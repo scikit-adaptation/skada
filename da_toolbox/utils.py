6a6ec75bfd313fcb3c4337e2ae0cba19ea0961fe
Theo Gnassounou <tgnassou@drago5.saclay.inria.fr>
2022-10-24 14:19:48 +0200

    add function for OT distance

--- da_toolbox/utils.py
+++ da_toolbox/utils.py
@@ -1,5 +1,7 @@
 import torch
 
+import ot
+
 from torch import nn
 from torch.utils.data import Dataset
 
@@ -34,6 +36,36 @@ def register_forwards_hook(model, intermediate_layers, layer_names):
             layer_module.register_forward_hook(
                 get_intermediate_layers(intermediate_layers, layer_name)
             )
+
+
+def ot_solve(a, b, M, num_iter_max=100000):
+    a2 = a.detach().cpu().numpy()  # .astype(np.float64)
+    b2 = b.detach().cpu().numpy()  # .astype(np.float64)
+    M2 = M.detach().cpu().numpy()  # .astype(np.float64)
+
+    # project on simplex for float64 or else numerical errors
+    a2 /= a2.sum()
+    b2 /= b2.sum()
+
+    G = ot.emd(a2, b2, M2, log=False, numItermax=num_iter_max)
+    return torch.from_numpy(G).to(a.device)
+
+
+def distance_matrix(gs, gt, ys, ft, alpha, beta, class_weights, n_classes):
+
+    weights = torch.Tensor(class_weights).to(gs.device)
+
+    dist = torch.cdist(gs, gt, p=2) ** 2
+
+    onehot_ys = torch.nn.functional.one_hot(ys, num_classes=n_classes).to(
+        device=ys.device, dtype=gs.dtype
+    )
+    loss_target = (weights @ onehot_ys.T).reshape(len(ys), 1) * (
+        -(onehot_ys @ ft.T) + torch.logsumexp(ft, dim=1)
+    )
+    M = alpha * dist + beta * loss_target
+
+    return M
 
 
 class NeuralNetwork(nn.Module):
