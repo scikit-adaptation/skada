032b6d118a77613c4ed887d5185dcd9137af70e1
Theo Gnassounou <tgnassou@drago5.saclay.inria.fr>
2022-10-26 17:12:22 +0200

    change name of dataset from moons

--- skada/datasets/__init__.py
+++ skada/datasets/__init__.py
@@ -1,9 +1,9 @@
 from ._samples_generator import (
-    make_shifted_blobs, make_shifted_datasets, make_out_of_distribution_dataset
+    make_shifted_blobs, make_shifted_datasets, make_dataset_from_moons_distribution
 )
 
 __all__ = [
     'make_shifted_blobs',
     'make_shifted_datasets',
-    'make_out_of_distribution_dataset'
+    'make_dataset_from_moons_distribution'
 ]
