1d08748e034061eb4607ae38cefccc987071531c
Theo Gnassounou <tgnassou@drago5.saclay.inria.fr>
2022-11-07 11:31:52 +0100

    change name of OT methods

--- skada/da/_mapping.py
+++ skada/da/_mapping.py
@@ -4,10 +4,10 @@
 
 from ot import da
 
-from .base import BaseDataAdaptEstimator
+from .base import BaseDataAdaptEstimator, clone
 
 
-class EMDTransport(BaseDataAdaptEstimator):
+class OTmapping(BaseDataAdaptEstimator):
     """Domain Adaptation Using Optimal Transport.
 
     Parameters
@@ -17,7 +17,7 @@ class EMDTransport(BaseDataAdaptEstimator):
 
     Attributes
     ----------
-    ot_emd_ : object
+    ot_transport_ : object
         The OT object based on Earth Mover's distance
         fitted on the source and target data.
 
@@ -56,7 +56,7 @@ class EMDTransport(BaseDataAdaptEstimator):
         weights : array-like, shape (n_samples,)
             The weights of the samples.
         """
-        X_ = self.ot_emd_.transform(Xs=X)
+        X_ = self.ot_transport_.transform(Xs=X)
         weights = None
         return X_, y, weights
 
@@ -79,12 +79,12 @@ class EMDTransport(BaseDataAdaptEstimator):
         self : object
             Returns self.
         """
-
-        self.ot_emd_ = da.EMDTransport().fit(Xs=X, Xt=X_target)
+        self.ot_transport_ = clone(da.EMDTransport())
+        self.ot_transport_.fit(Xs=X, Xt=X_target)
         return self
 
 
-class SinkhornTransport(BaseDataAdaptEstimator):
+class EntropicOTmapping(BaseDataAdaptEstimator):
     """Domain Adaptation Using Optimal Transport.
 
     Parameters
@@ -96,7 +96,7 @@ class SinkhornTransport(BaseDataAdaptEstimator):
 
     Attributes
     ----------
-    ot_sinkhorn_ : object
+    ot_transport_ : object
         The OT object based on Sinkhorn Algorithm
         fitted on the source and target data.
 
@@ -137,7 +137,7 @@ class SinkhornTransport(BaseDataAdaptEstimator):
         weights : array-like, shape (n_samples,)
             The weights of the samples.
         """
-        X_ = self.ot_sinkhorn_.transform(Xs=X)
+        X_ = self.ot_transport_.transform(Xs=X)
         weights = None
         return X_, y, weights
 
@@ -161,13 +161,12 @@ class SinkhornTransport(BaseDataAdaptEstimator):
             Returns self.
         """
 
-        self.ot_sinkhorn_ = da.SinkhornTransport(
-            reg_e=self.reg_e
-        ).fit(Xs=X, Xt=X_target)
+        self.ot_transport_ = clone(da.SinkhornTransport(reg_e=self.reg_e))
+        self.ot_transport_.fit(Xs=X, Xt=X_target)
         return self
 
 
-class SinkhornLpl1Transport(BaseDataAdaptEstimator):
+class ClassRegularizerOTmapping(BaseDataAdaptEstimator):
     """Domain Adaptation Using Optimal Transport.
 
     Parameters
@@ -178,10 +177,12 @@ class SinkhornLpl1Transport(BaseDataAdaptEstimator):
         Entropic regularization parameter.
     reg_cl : float, default=0.1
         Class regularization parameter.
+    norm : tuple, default="Lpl1"
+        Norm use for the regularizer of the class labels.
 
     Attributes
     ----------
-    ot_lpl1_ : object
+    ot_transport_ : object
         The OT object based on Sinkhorn Algorithm
         + LpL1 class regularization fitted on the source
         and target data.
@@ -196,11 +197,13 @@ class SinkhornLpl1Transport(BaseDataAdaptEstimator):
         self,
         base_estimator,
         reg_e=1,
-        reg_cl=0.1
+        reg_cl=0.1,
+        norm="Lpl1"
     ):
         super().__init__(base_estimator)
         self.reg_e = reg_e
         self.reg_cl = reg_cl
+        self.norm = norm
 
     def predict_adapt(self, X, y, X_target, y_target=None):
         """Predict adaptation (weights, sample or labels).
@@ -225,7 +228,7 @@ class SinkhornLpl1Transport(BaseDataAdaptEstimator):
         weights : None
             No weights is this case.
         """
-        X_ = self.ot_lpl1_.transform(Xs=X)
+        X_ = self.ot_transport_.transform(Xs=X)
         weights = None
         return X_, y, weights
 
@@ -248,94 +251,16 @@ class SinkhornLpl1Transport(BaseDataAdaptEstimator):
         self : object
             Returns self.
         """
-        self.ot_lpl1_ = da.SinkhornLpl1Transport(
-            reg_e=self.reg_e, reg_cl=self.reg_cl
-        ).fit(Xs=X, ys=y, Xt=X_target)
-        return self
+        assert self.norm in ["Lpl1", "L1l2"], "Unknown norm"
 
+        if self.norm == "Lpl1":
+            self.ot_transport_ = clone(da.SinkhornLpl1Transport(
+                reg_e=self.reg_e, reg_cl=self.reg_cl
+            ))
+        elif self.norm == "L1l2":
+            self.ot_transport_ = clone(da.SinkhornL1l2Transport(
+                reg_e=self.reg_e, reg_cl=self.reg_cl
+            ))
 
-class SinkhornL1l2Transport(BaseDataAdaptEstimator):
-    """Domain Adaptation Using Optimal Transport.
-
-    Parameters
-    ----------
-    base_estimator : estimator object
-        The base estimator to fit on reweighted data.
-    reg_e : float, default=1
-        Entropic regularization parameter.
-    reg_cl : float, default=0.1
-        Class regularization parameter.
-
-    Attributes
-    ----------
-    ot_l1l2_ : object
-        The OT object based on Sinkhorn Algorithm
-        + L1L2 class regularization fitted on the source
-        and target data.
-
-    References
-    ----------
-    .. [1] N. Courty, R. Flamary, D. Tuia and A. Rakotomamonjy,
-           Optimal Transport for Domain Adaptation, in IEEE
-           Transactions on Pattern Analysis and Machine Intelligence
-    """
-    def __init__(
-        self,
-        base_estimator,
-        reg_e=1,
-        reg_cl=0.1
-    ):
-        super().__init__(base_estimator)
-        self.reg_e = reg_e
-        self.reg_cl = reg_cl
-
-    def predict_adapt(self, X, y, X_target, y_target=None):
-        """Predict adaptation (weights, sample or labels).
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples, n_features)
-            The source data.
-        y : array-like, shape (n_samples,)
-            The source labels.
-        X_target : array-like, shape (n_samples, n_features)
-            The target data.
-        y_target : array-like, shape (n_samples,), optional
-            The target labels.
-
-        Returns
-        -------
-        X_t : array-like, shape (n_samples, n_components)
-            The data transformed to the target subspace.
-        y_t : array-like, shape (n_samples,)
-            The labels (same as y).
-        weights : array-like, shape (n_samples,)
-            The weights of the samples.
-        """
-        X_ = self.ot_l1l2_.transform(Xs=X)
-        weights = None
-        return X_, y, weights
-
-    def fit_adapt(self, X, y, X_target, y_target=None):
-        """Fit adaptation parameters.
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples, n_features)
-            The source data.
-        y : array-like, shape (n_samples,)
-            The source labels.
-        X_target : array-like, shape (n_samples, n_features)
-            The target data.
-        y_target : array-like, shape (n_samples,), optional
-            The target labels.
-
-        Returns
-        -------
-        self : object
-            Returns self.
-        """
-        self.ot_l1l2_ = da.SinkhornL1l2Transport(
-            reg_e=self.reg_e, reg_cl=self.reg_cl
-        ).fit(Xs=X, ys=y, Xt=X_target)
+        self.ot_transport_.fit(Xs=X, ys=y, Xt=X_target)
         return self
