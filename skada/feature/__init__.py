b74c88923c59c4304b4584eb388c04230c6633e7
tgnassou <theo.gnassounou@gmail.com>
2022-12-05 18:13:33 +0100

    fix flake8?

--- skada/feature/__init__.py
+++ skada/feature/__init__.py
@@ -4,7 +4,7 @@ Some methods for deep domain adaptation.
 try:
     import torch  # noqa: F401
     import skorch  # noqa: F401
-except ImportError as e:
+except ImportError as e :
     print("ERROR : torch and skorch are required for importing feature's method.")
     raise(e)
 
