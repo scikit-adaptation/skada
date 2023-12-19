fc0ff19700b47d820f45b7c063b1a67583a9ef0e
Rémi Flamary <remi.flamary@gmail.com>
2023-12-19 10:01:43 +0100

    pep8

--- examples/plot_skada_logo.py
+++ examples/plot_skada_logo.py
@@ -20,7 +20,7 @@ matplotlib and plotting the solution of the EMD solver from POT.
 import numpy as np
 import scipy as sp
 import pylab as pl
-from scipy.spatial.transform import Rotation as R    
+from scipy.spatial.transform import Rotation as R
 
 import matplotlib.pyplot as plt
 plt.rcParams.update({
@@ -28,116 +28,128 @@ plt.rcParams.update({
     "font.family": "Computer Modern"})
 
 
-def get_lda_interp(xs,xt,alpha,len_=1):
+def get_lda_interp(xs, xt, alpha, len_=1):
     """Compute the LDA interpolation between two domains"""
-    
+
     # compute means (clases assumed to be balanced)
-    m1 = (1-alpha)*xs[:n//2,:].mean(0)+(alpha)*xt[:n//2,:].mean(0)
-    m2 = (1-alpha)*xs[n//2:,:].mean(0)+(alpha)*xt[n//2:,:].mean(0)
-    
+    m1 = (1-alpha)*xs[:n//2, :].mean(0)+(alpha)*xt[:n//2, :].mean(0)
+    m2 = (1-alpha)*xs[n//2:, :].mean(0)+(alpha)*xt[n//2:, :].mean(0)
+
     vo = m2-m1
     vo = vo / np.linalg.norm(vo)
-    
+
     theta = np.pi/2
     rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
-    
+
     vor = rot.dot(vo)
-                    
+
     mm = 0.5*m1+0.5*m2
-    
-    return np.vstack((mm-len_*vor,mm+len_*vor))
-    
+
+    return np.vstack((mm-len_*vor, mm+len_*vor))
+
 # %% Generate Data
-    
+
+
 n = 40
 
 np.random.seed(42)
 
-xs = 0.2*np.random.randn(n,2)
+xs = 0.2*np.random.randn(n, 2)
 
 # class  specific change
-xs[n//2:,0]+=0.5
-xs[n//2:,1]-=1
+xs[n//2:, 0] += 0.5
+xs[n//2:, 1] -= 1
 
 # classes 0 an 3 for blue/red colors
-ys=np.zeros(n)
-ys[n//2:]=3
+ys = np.zeros(n)
+ys[n//2:] = 3
 
 # global changes
-xt = 0.15*np.random.randn(n,2)
-xt[:,0]+=0.7
-xt[:,1]-=0
+xt = 0.15*np.random.randn(n, 2)
+xt[:, 0] += 0.7
+xt[:, 1] -= 0
 
 # class specific change
-xt[n//2:,0]+=1
-xt[n//2:,1]-=0.2
+xt[n//2:, 0] += 1
+xt[n//2:, 1] -= 0.2
 
 # class 7 for gray color (target without label)
-yt=np.ones(n)*7
+yt = np.ones(n)*7
 
 
 # %% Plot the smal logo
 nb = 10
 alpha0 = 0.2
 
-alphalist = np.linspace(0,1,nb)
+alphalist = np.linspace(0, 1, nb)
 
-pl.figure(1,(2,2))
+pl.figure(1, (2, 2))
 pl.clf()
 alpha = 0.7
 
 # plot samples
-pl.scatter(xs[ys==0,0],xs[ys==0,1],c='C0',cmap='tab10',vmax=10,alpha=alpha,label='Source data',marker='o')
-pl.scatter(xs[ys==3,0],xs[ys==3,1],c='C3',cmap='tab10',vmax=10,alpha=alpha,label='Source data',marker='s')
-pl.scatter(xt[ys==0,0],xt[ys==0,1],c='C7',cmap='tab10',vmax=10,alpha=alpha,label='Source data',marker='o')
-pl.scatter(xt[ys==3,0],xt[ys==3,1],c='C7',cmap='tab10',vmax=10,alpha=alpha,label='Source data',marker='s')
+pl.scatter(xs[ys == 0, 0], xs[ys == 0, 1], c='C0', cmap='tab10',
+           vmax=10, alpha=alpha, label='Source data', marker='o')
+pl.scatter(xs[ys == 3, 0], xs[ys == 3, 1], c='C3', cmap='tab10',
+           vmax=10, alpha=alpha, label='Source data', marker='s')
+pl.scatter(xt[ys == 0, 0], xt[ys == 0, 1], c='C7', cmap='tab10',
+           vmax=10, alpha=alpha, label='Source data', marker='o')
+pl.scatter(xt[ys == 3, 0], xt[ys == 3, 1], c='C7', cmap='tab10',
+           vmax=10, alpha=alpha, label='Source data', marker='s')
 
 # plot classifiers
-ax=pl.axis()
+ax = pl.axis()
 for i in range(nb):
-    xclass = get_lda_interp(xs,xt,alphalist[i],2)
-    pl.plot(xclass[:,0],xclass[:,1],color='C2',alpha=alpha0+(1-alpha0)*alphalist[i], zorder=0,linewidth=2)
+    xclass = get_lda_interp(xs, xt, alphalist[i], 2)
+    pl.plot(xclass[:, 0], xclass[:, 1], color='C2', alpha=alpha0 +
+            (1-alpha0)*alphalist[i], zorder=0, linewidth=2)
 pl.axis(ax)
 pl.axis('off')
 
 # save to file
-pl.savefig('skada_logo.pdf',bbox_inches='tight')
-pl.savefig('skada_logo.svg',bbox_inches='tight',dpi=400)
-pl.savefig('skada_logo.png',bbox_inches='tight',dpi=400) 
-        
+pl.savefig('skada_logo.pdf', bbox_inches='tight')
+pl.savefig('skada_logo.svg', bbox_inches='tight', dpi=400)
+pl.savefig('skada_logo.png', bbox_inches='tight', dpi=400)
 
-#%%
+
+# %%
 
 # target data for full logo
 xt2 = xt.copy()
-xt2[:,0]+=6.3
+xt2[:, 0] += 6.3
 
 
 nb = 10
 alpha0 = 0.2
-alphalist = np.linspace(0,1,nb)
+alphalist = np.linspace(0, 1, nb)
 
-pl.figure(2,(9,1.5))
+pl.figure(2, (9, 1.5))
 pl.clf()
 alpha = 0.7
 
 # plot samples
-pl.scatter(xs[ys==0,0],xs[ys==0,1],c='C0',cmap='tab10',vmax=10,alpha=alpha,label='Source data',marker='o')
-pl.scatter(xs[ys==3,0],xs[ys==3,1],c='C3',cmap='tab10',vmax=10,alpha=alpha,label='Source data',marker='s')
-pl.scatter(xt2[ys==0,0],xt2[ys==0,1],c='C7',cmap='tab10',vmax=10,alpha=alpha,label='Source data',marker='o')
-pl.scatter(xt2[ys==3,0],xt2[ys==3,1],c='C7',cmap='tab10',vmax=10,alpha=alpha,label='Source data',marker='s')
+pl.scatter(xs[ys == 0, 0], xs[ys == 0, 1], c='C0', cmap='tab10',
+           vmax=10, alpha=alpha, label='Source data', marker='o')
+pl.scatter(xs[ys == 3, 0], xs[ys == 3, 1], c='C3', cmap='tab10',
+           vmax=10, alpha=alpha, label='Source data', marker='s')
+pl.scatter(xt2[ys == 0, 0], xt2[ys == 0, 1], c='C7', cmap='tab10',
+           vmax=10, alpha=alpha, label='Source data', marker='o')
+pl.scatter(xt2[ys == 3, 0], xt2[ys == 3, 1], c='C7', cmap='tab10',
+           vmax=10, alpha=alpha, label='Source data', marker='s')
 
 # plot classifiers
-ax=pl.axis()
+ax = pl.axis()
 for i in range(nb):
-    xclass = get_lda_interp(xs,xt2,alphalist[i],2)
-    pl.plot(xclass[:,0],xclass[:,1],color='C2',alpha=alpha0+(1-alpha0)*alphalist[i], zorder=0,linewidth=2)
-pl.text(1.3,-1.18,r'\bf\textsf{SKADA}',fontsize=80,usetex=True,zorder=0.5, color=(0.2,0.2,0.2))
+    xclass = get_lda_interp(xs, xt2, alphalist[i], 2)
+    pl.plot(xclass[:, 0], xclass[:, 1], color='C2', alpha=alpha0 +
+            (1-alpha0)*alphalist[i], zorder=0, linewidth=2)
+pl.text(1.3, -1.18, r'\bf\textsf{SKADA}', fontsize=80,
+        usetex=True, zorder=0.5, color=(0.2, 0.2, 0.2))
 
 #pl.scatter(xcs[:,0],xcs[:,1],c='gray',cmap='tab10',vmin=0,vmax=10,alpha=0.5,label='Target data')
 pl.axis(ax)
 pl.axis('off')
 
-pl.savefig('skada_logo_full.pdf',bbox_inches='tight')
-pl.savefig('skada_logo_full.svg',bbox_inches='tight',dpi=400)
-pl.savefig('skada_logo_full.png',bbox_inches='tight',dpi=400) 
+pl.savefig('skada_logo_full.pdf', bbox_inches='tight')
+pl.savefig('skada_logo_full.svg', bbox_inches='tight', dpi=400)
+pl.savefig('skada_logo_full.png', bbox_inches='tight', dpi=400)
