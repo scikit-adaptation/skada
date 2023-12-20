c4830e7a770f4fcbef3bec1e5e729917f24153fa
Oleksii Kachaiev <kachayev@gmail.com>
2023-12-20 22:30:00 +0100

    Fix new tests

--- skada/datasets/tests/test_base.py
+++ skada/datasets/tests/test_base.py
@@ -12,7 +12,7 @@ def test_dataset_train_label_masking():
     dataset = DomainAwareDataset()
     dataset.add_domain(np.array([1., 2.]), np.array([1, 2]), 's1')
     dataset.add_domain(np.array([10., 20., 30.]), np.array([10, 20, 30]), 't1')
-    X, y, sample_domain = dataset.pack_for_train(as_sources=['s1'], as_targets=['t1'])
+    X, y, sample_domain = dataset.pack_train(as_sources=['s1'], as_targets=['t1'])
 
     # test shape of the output
     assert X.shape == (5,)
@@ -26,12 +26,12 @@ def test_dataset_train_label_masking():
     assert np.all(y[sample_domain > 0] > 0)
 
     # custom mask
-    X, y, sample_domain = dataset.pack_for_train(
+    X, y, sample_domain = dataset.pack_train(
         as_sources=['s1'], as_targets=['t1'], mask=-10)
     assert_array_equal(y[sample_domain < 0], np.array([-10, -10, -10]))
 
     # test packing does not perform masking
-    X, y, sample_domain = dataset.pack_for_test(as_targets=['t1'])
+    X, y, sample_domain = dataset.pack_test(as_targets=['t1'])
     assert X.shape == (3,)
     assert y.shape == (3,)
     assert sample_domain.shape == (3,)
