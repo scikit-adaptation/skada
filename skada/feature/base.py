2dc1741b49ce2f684d4c0c89581fdcaf16e7827c
tgnassou <theo.gnassounou@gmail.com>
2024-01-17 17:41:21 +0100

    add wrapper for neural net"

--- skada/feature/base.py
+++ skada/feature/base.py
@@ -4,13 +4,28 @@
 # License: BSD 3-Clause
 
 
+from abc import abstractmethod
+
 import torch
 from torch.utils.data import DataLoader, Sampler
+from skorch import NeuralNetClassifier
 
 from .utils import _register_forwards_hook
 
 
 class DomainAwareCriterion(torch.nn.Module):
+    """Criterion for domain aware loss
+
+    Parameters
+    ----------
+    criterion : torch criterion (class)
+        The initialized criterion (loss) used to optimize the
+        module with prediction on source.
+    dacriterion : torch criterion (class)
+        The initialized criterion (loss) used to compute the
+        loss to reduce the divergence between domains.
+    """
+
     def __init__(self, criterion, dacriterion):
         super(DomainAwareCriterion, self).__init__()
         self.criterion = criterion
@@ -21,21 +36,54 @@ class DomainAwareCriterion(torch.nn.Module):
         y_pred,
         y_true,
     ):
-        # choose source or target domain
-        y_pred, features, sample_domain = y_pred
-        ys = y_pred[sample_domain > 0]
-        yt = y_pred[sample_domain < 0]
+        y_pred, y_pred_domain, features, sample_domain = y_pred  # unpack
+        y_pred_s = y_pred[sample_domain > 0]
+        y_pred_t = y_pred[sample_domain < 0]
+
+        if y_pred_domain is not None:
+            y_pred_domain_s = y_pred_domain[sample_domain > 0]
+            y_pred_domain_t = y_pred_domain[sample_domain < 0]
 
         features_s = features[sample_domain > 0]
         features_t = features[sample_domain < 0]
 
         # predict
-        return self.criterion(ys, y_true[sample_domain > 0]) + self.dacriterion(
-            yt, features_s, features_t
+        return self.criterion(y_pred_s, y_true[sample_domain > 0]) + self.dacriterion(
+            y_pred_s, y_pred_t, y_pred_domain_s, y_pred_domain_t, features_s, features_t
         )
 
 
+class BaseDACriterion(torch.nn.Module):
+    def __init__(
+        self,
+    ):
+        super(BaseDACriterion, self).__init__()
+
+    @abstractmethod
+    def forward(
+        self,
+        y_pred_s,
+        y_pred_t,
+        y_pred_domain_s,
+        y_pred_domain_t,
+        features_s,
+        features_t,
+    ):
+        """Compute the domain adaptation loss"""
+        pass
+
+
 class DomainBalancedSampler(Sampler):
+    """Domain balanced sampler
+
+    A sampler to have as much as source and target in the batch.
+
+    Parameters
+    ----------
+    dataset : torch dataset
+        The dataset to sample from.
+    """
+
     def __init__(self, dataset):
         self.dataset = dataset
         self.positive_indices = [
@@ -75,6 +123,16 @@ class DomainBalancedSampler(Sampler):
 
 
 class DomainBalancedDataLoader(DataLoader):
+    """Domain balanced data loader
+
+    A data loader to have as much as source and target in the batch.
+
+    Parameters
+    ----------
+    dataset : torch dataset
+        The dataset to sample from.
+    """
+
     def __init__(
         self,
         dataset,
@@ -108,9 +166,23 @@ class DomainBalancedDataLoader(DataLoader):
 
 
 class DomainAwareModule(torch.nn.Module):
-    def __init__(self, module, layer_name):
+    """Domain aware module
+
+    A domain aware module allowing to separe the source and target and
+    compute their respective prediction and feaures.
+
+    Parameters
+    ----------
+    module : torch module
+        The module to use.
+    layer_name : str
+        The name of the layer to extract the features from.
+    """
+
+    def __init__(self, module, layer_name, domain_classifier=None):
         super(DomainAwareModule, self).__init__()
         self.module_ = module
+        self.domain_classifier_ = domain_classifier
         self.layer_name = layer_name
         self.intermediate_layers = {}
         _register_forwards_hook(
@@ -119,15 +191,66 @@ class DomainAwareModule(torch.nn.Module):
 
     def forward(self, X, sample_domain):
         # choose source or target domain
-        Xs = X[sample_domain > 0]
-        Xt = X[sample_domain < 0]
+        X_s = X[sample_domain > 0]
+        X_t = X[sample_domain < 0]
         # predict
-        ys = self.module_(Xs)
+        y_pred_s = self.module_(X_s)
         features_s = self.intermediate_layers[self.layer_name]
-        yt = self.module_(Xt)
+        y_pred_t = self.module_(X_t)
         features_t = self.intermediate_layers[self.layer_name]
+
+        if self.domain_classifier_ is not None:
+            y_pred_domain_s = self.domain_classifier_(features_s)
+            y_pred_domain_t = self.domain_classifier_(features_t)
+            y_pred_domain = torch.cat((y_pred_domain_s, y_pred_domain_t), dim=0)
+        else:
+            y_pred_domain = None
         return (
-            torch.cat((ys, yt), dim=0),  # prediction
-            torch.cat((features_s, features_t), dim=0),  # feature
-            sample_domain,  # domain
+            torch.cat((y_pred_s, y_pred_t), dim=0),  # predictions
+            y_pred_domain,  # domain predictions
+            torch.cat((features_s, features_t), dim=0),  # features
+            sample_domain,  # domains
         )
+
+
+class DomainAwareNet(NeuralNetClassifier):
+    """Domain aware net
+
+    A wrapper for NeuralNetClassifier to give a dict as input to the fit.
+
+    Parameters
+    ----------
+    module : torch module
+        The module to use.
+    layer_name : str
+        The name of the layer to extract the features from.
+    domain_classifier : torch module
+        The domain classifier to use.
+    **kwargs : dict
+        Keyword arguments passed to the skorch NeuralNetClassifier class.
+    """
+
+    def __init__(self, module, layer_name, domain_classifier=None, **kwargs):
+        super().__init__(
+            DomainAwareModule(module, layer_name, domain_classifier), **kwargs
+        )
+
+    def fit(self, X, y, sample_domain=None, **fit_params):
+        """Fit the model
+
+        Parameters
+        ----------
+        X : dict or torch tensor
+            The input data. If a dict, it should contain a key 'X' with the
+            input data and a key 'sample_domain' with the domain of each
+            sample.
+        y : torch tensor
+            The target data.
+        sample_domain : torch tensor
+            The domain of each sample.
+        """
+        if not isinstance(X, dict):
+            X = {"X": X}
+            X["sample_domain"] = sample_domain
+
+        return super().fit(X, y, **fit_params)
