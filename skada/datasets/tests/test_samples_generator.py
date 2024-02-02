5fb41626d0805f016007feca27299ddc2a40027f
Rouxben <61913555+Rouxben@users.noreply.github.com>
2024-02-02 15:53:22 +0100

    updated the test
    this was needed with teh previous changes

--- skada/datasets/tests/test_samples_generator.py
+++ skada/datasets/tests/test_samples_generator.py
@@ -115,11 +115,16 @@ def test_make_shifted_datasets(shift):
     X_source, X_target, y_source, y_target = source_target_split(
         X, y, sample_domain=sample_domain
     )
-
-    assert X_source.shape == (10 * 8 // 2, 2), "X source shape mismatch"
+    if shift == "subspace":
+        assert X_source.shape == (10 * 8 // 2, 2), "X source shape mismatch"
+    else:
+        assert X_source.shape == (10 * 8, 2), "X source shape mismatch"
     assert y_source.shape == (10 * 8,), "y source shape mismatch"
     assert np.unique(y_source).shape == (2,), "Unexpected number of cluster"
-    assert X_target.shape == (10 * 8 // 2, 2), "X target shape mismatch"
+    if shift == "subspace":
+        assert X_target.shape == (10 * 8 // 2, 2), "X target shape mismatch"
+    else :
+        assert X_target.shape == (10 * 8, 2), "X target shape mismatch"
     assert y_target.shape == (10 * 8,), "y target shape mismatch"
     assert np.unique(y_target).shape == (2,), "Unexpected number of cluster"
 
