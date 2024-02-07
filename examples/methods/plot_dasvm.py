ac330d3af84ea4b184f795d66d1514dd3754ff91
Rouxben <61913555+Rouxben@users.noreply.github.com>
2024-02-07 10:28:42 +0100

    started to implement dasvm method
    there is a start to the implementation of dasvm, and an exemple of it

---
+++ examples/methods/plot_dasvm.py
@@ -0,0 +1,59 @@
+import numpy as np
+from sklearn.pipeline import make_pipeline
+import matplotlib.pyplot as plt
+import math
+
+from skada.datasets import make_shifted_datasets
+from skada._dasvm import BaseDasvmAdapter
+from skada.utils import check_X_y_domain, source_target_split
+
+
+from sklearn.svm import SVC
+
+
+
+
+
+xlim = (-2.2, 4.2)
+ylim = (-2, 4.2)
+
+figure, axis = plt.subplots(1, 2)
+
+X, y, sample_domain = make_shifted_datasets(
+    n_samples_source=60,
+    n_samples_target=40,
+    shift="covariate_shift",
+    noise=None,
+    label="binary",
+)
+X, y, sample_domain = check_X_y_domain(X, y, sample_domain)
+Xs, Xt, ys, yt = source_target_split(
+    X, y, sample_domain=sample_domain
+)
+
+
+axis[0].scatter(Xs[:,0], Xs[:,1], c=ys)
+axis[0].set_xlim(xlim)
+axis[0].set_ylim(ylim)
+
+axis[1].scatter(Xt[:,0], Xt[:,1], c=yt)
+axis[1].set_xlim(xlim)
+axis[1].set_ylim(ylim)
+
+E = BaseDasvmAdapter(k = 5).fit(X, y, sample_domain)
+
+
+figure, axis = plt.subplots(1, 2)
+a = []
+for i in [0, -1]:
+	e = (SVC(gamma='auto').fit(Xs, ys) if i == 0 else E.get_estimator())
+	x_points = np.linspace(xlim[0], xlim[1], 200)
+	y_points = np.linspace(ylim[0], ylim[1], 200)
+	# Plotting a red hyperplane
+	X = np.array([[x, y] for x in x_points for y in y_points])
+	a.append(axis[i].scatter(X[:,0], X[:,1], c=e.decision_function(X)))
+	axis[i].set_xlim(xlim)
+	axis[i].set_ylim(ylim)
+figure.colorbar(a[-1])
+plt.show()
+
---
+++ skada/_dasvm.py
@@ -0,0 +1,188 @@
+import numpy as np
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import math
+
+from skada.base import BaseEstimator, BaseAdapter, BaseSelector
+from skada.utils import check_X_y_domain, source_target_split
+
+from sklearn.svm import SVC
+
+
+
+
+class BaseDasvmAdapter(BaseAdapter, BaseSelector):
+    """Base class for all DA estimators implemented using OT mapping.
+
+    Each implementation has to provide `_create_transport_estimator` callback
+    to create OT object using parameters saved in the constructor.
+    """
+    def __init__(self, base_estimator: BaseEstimator = SVC(gamma='auto'), k = 3, Stop = 1_000, **kwargs):
+        BaseSelector.__init__(self, base_estimator, **kwargs)
+        self.Stop = Stop
+        self.k = k
+
+    def _find_points_next_step(self, I_list, d, c):
+        I = np.array([], dtype=int)
+        # We should take k points for each of the c classes, depending on the values of d
+        for j in range(min(self.k, math.ceil(d.shape[0]/c))):
+            mask = np.ones(d.shape[0], dtype=bool)
+            mask[I] = False
+            I_ = np.unique(np.argmax(d[mask], axis=0))
+            # We need to get all those indices to be take into account the fact that the some previous points weren't in the list
+            for l in I:
+                I_[I_>=l]+=1
+            I = np.concatenate((I, I_))
+            I = np.sort(I) # we sort I as it is an assumption for I to be sorted for the previous algorithm
+    
+        # Again we need to get all those indices to be take into account the fact that the some previous points weren't in the list
+        for l in range(len(I_list[-1])):
+            if I_list[-1][l]:
+                I[I>=l]+=1
+    
+        I_list.append(np.copy(I_list[-1]))
+        for l in I:
+            if I_list[-1][l]: 
+                raise ValueError(f"problem here: {l}")
+            I_list[-1][l] = True
+
+
+    def fit(self, X, y=None, sample_domain=None):
+        """Fit adaptation parameters.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The source data.
+        y : array-like, shape (n_samples,)
+            The source labels.
+        sample_domain : array-like, shape (n_samples,)
+            The domain labels (same as sample_domain).
+
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
+        X, y, sample_domain = check_X_y_domain(X, y, sample_domain)
+        Xs, Xt, ys, _ = source_target_split(
+            X, y, sample_domain=sample_domain
+        )
+
+
+
+        n = Xs.shape[0]
+        m = Xt.shape[0]
+        c = np.unique(ys).shape[0] # number of classes
+    
+        # This is the list of the indices from the points from Xs that have been discarded
+        Id = [np.array([False]*n)]
+        # This is the list of the indices from the points from Xt that have been added
+        Ia = [np.array([False]*m)]
+    
+        # In the first step, the SVC is fitted on the source
+        X = Xs
+        y = ys
+        Estimators = [make_pipeline(StandardScaler(), self.base_estimator)]
+        Estimators[-1].fit(X, y)
+    
+        # We need to look at the decision function for to select the labaled data that we discard and the semi-labeled points that we will add
+    
+        # look at those that have not been discarded
+        decisions_s = Estimators[-1].decision_function(X[~Id[-1]])
+        if c==2: 
+            decisions_s = np.array([-decisions_s, decisions_s]).T
+        # look at those that haven't been added
+        decisions_ta = Estimators[-1].decision_function(Xt[~Ia[-1]])
+        if c==2: 
+            decisions_ta = np.array([-decisions_ta, decisions_ta]).T
+        
+        # We want to take values that are unsure, meaning we want those that have 
+        # values the closest that we can to c-1 (to 0 when label='binary')
+        decisions_ta = -np.abs(decisions_ta-c-1)
+    
+        # doing the selection on the labeled data
+        self._find_points_next_step(Id, decisions_s, c)
+        # doing the selection on the semi-labeled data
+        self._find_points_next_step(Ia, decisions_ta, c)
+    
+        i = 0
+        while (sum(Ia[-1])<m or sum(Id[-1])<n) and i<self.Stop:
+            i+=1
+            mask = np.ones(Xs.shape[0], dtype=bool)
+            mask[Id[-1]] = False
+            X = np.concatenate((Xs[mask], Xt[Ia[-1]]))
+            y = np.concatenate((ys[mask], Estimators[-1].predict(Xt[Ia[-1]])))
+    
+            Estimators.append(make_pipeline(StandardScaler(), self.base_estimator))
+            Estimators[-1].fit(X, y)
+    
+            for j in range(len(Ia[-1])):
+                if Ia[-1][j]:
+                    x = Xt[j]
+                    if Estimators[-1].predict([x])!=Estimators[-2].predict([x]):
+                        if not Ia[-1][j]:
+                            raise ValueError("There is a problem here...")
+                        Ia[-1][j] = False
+            
+            #look at those that have not been discarded
+            X_ = Xs[~Id[-1]]
+            decisions_s = (Estimators[-1].decision_function(X_) if X_.shape[0] else X_)
+            if c==2: 
+                decisions_s = np.array([-decisions_s, decisions_s]).T
+            #look at those that haven't been added
+            X_ = Xt[~Ia[-1]]
+            decisions_ta = (Estimators[-1].decision_function(X_) if X_.shape[0] else X_)
+            if c==2: 
+                decisions_ta = np.array([-decisions_ta, decisions_ta]).T
+    
+            # We want to take values that are unsure, meaning we want those that have 
+            # values the closest that we can to c-1 (to 0 when label='binary', or 4 when is its 'multiclass')
+            decisions_ta = -np.abs(decisions_ta-c-1)
+        
+            # doing the selection on the labeled data
+            self._find_points_next_step(Id, decisions_s, c)
+            # doing the selection on the semi-labeled data
+            self._find_points_next_step(Ia, decisions_ta, c)
+        
+    
+        #On last fit only on semi-labeled data is looked at
+        mask = np.ones(Xs.shape[0], dtype=bool)
+        mask[Id[-1]] = False
+        X = np.concatenate((Xs[mask], Xt[Ia[-1]]))
+        y = np.concatenate((ys[mask], Estimators[-1].predict(Xt[Ia[-1]])))
+    
+        Estimators.append(make_pipeline(StandardScaler(), SVC(gamma='auto')))
+        Estimators[-1].fit(X, y)
+        
+        self.base_estimator_ = Estimators[-1]
+
+        # t could be interesting to return multiple estimators, or the list Ia and Id (this making them being an object attribute)
+        return self
+    
+
+    def adapt(self, X, y=None, sample_domain=None):
+        """
+        I was thinking of returning Xt and \hat{y}t
+        """
+        pass
+
+
+
+    def get_estimator(self, *params) -> BaseEstimator:
+        """Returns estimator associated with `params`.
+
+        The set of available estimators and access to them has to be provided
+        by specific implementations.
+        """
+        return self.base_estimator_
+
+
+
+    def _route_to_estimator(self, method_name, X, y=None, **params) -> np.ndarray:
+        """
+        def predict(self, X, **params):
+            return self._route_to_estimator('predict', X, **params)
+        """
+        pass
