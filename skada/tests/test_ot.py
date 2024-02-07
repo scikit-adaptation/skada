5080663ac665f33a4212a45172a40c93b71b6794
Rémi Flamary <remi.flamary@gmail.com>
2024-02-07 16:03:57 +0100

    add test!

---
+++ skada/tests/test_ot.py
@@ -0,0 +1,23 @@
+# Author: Remi Flamary <remi.flamary@polytechnique.edu>
+#
+# License: BSD 3-Clause
+
+
+from skada import JDOTRegressor
+from sklearn.linear_model import Ridge
+from skada.utils import source_target_split
+
+
+def test_JDOTRegressor(da_reg_dataset):
+
+    X, y, sample_domain = da_reg_dataset
+
+    Xs, Xt, ys, yt = source_target_split(X, y, sample_domain=sample_domain)
+
+    jdot = JDOTRegressor(base_estimator=Ridge())
+
+    jdot.fit(X, y, sample_domain)
+
+    ypred = jdot.predict(Xt)
+
+    assert ypred.shape[0] == Xt.shape[0]
