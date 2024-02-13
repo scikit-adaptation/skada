f93c5ad88224e053915edbbe52f43df6ed7671d8
tgnassou <theo.gnassounou@gmail.com>
2024-02-13 11:20:07 +0100

    add test_divergence

--- skada/feature/tests/test_divergence.py
+++ skada/feature/tests/test_divergence.py
@@ -1,68 +1,49 @@
 # Author: Theo Gnassounou <theo.gnassounou@inria.fr>
-#         Remi Flamary <remi.flamary@polytechnique.edu>
 #
 # License: BSD 3-Clause
 
+import numpy as np
 import torch
-from torch import nn
 
 import pytest
 
-from skada.feature import DeepCORAL, DAN
-from skada.feature._modules import ToyCNN
+from skada.feature import DeepCoral
+from skada.feature._modules import ToyModule
+from skada.datasets import make_shifted_datasets
 
 
-@pytest.mark.parametrize(
-    "input_size, n_channels, n_classes",
-    [(100, 2, 5), (120, 1, 3)],
-)
-def test_deepcoral(input_size, n_channels, n_classes):
-    module = ToyCNN(
-        n_channels=n_channels, input_size=input_size, n_classes=n_classes, kernel_size=8
-    )
+def test_deepcoral():
+    module = ToyModule()
     module.eval()
 
-    rng = torch.random.manual_seed(42)
     n_samples = 20
-    X = torch.randn(size=(n_samples, n_channels, input_size), generator=rng)
-    y = torch.randint(high=n_classes, size=(n_samples,), generator=rng)
-    X_target = torch.randn(size=(n_samples, n_channels, input_size), generator=rng)
-
-    method = DeepCORAL(
-        module=module,
-        criterion=nn.CrossEntropyLoss(),
-        layer_names=["feature_extractor"],
-        max_epochs=2
+    dataset = make_shifted_datasets(
+        n_samples_source=n_samples,
+        n_samples_target=n_samples,
+        shift="concept_drift",
+        noise=0.1,
+        random_state=42,
+        return_dataset=True,
     )
-    method.fit(X, y, X_target=X_target)
-    y_pred = method.predict(X_target)
 
-    assert y_pred.shape[0] == n_samples
-
-
-@pytest.mark.parametrize(
-    "input_size, n_channels, n_classes",
-    [(100, 2, 5), (120, 1, 3)],
-)
-def test_dan(input_size, n_channels, n_classes):
-    module = ToyCNN(
-        n_channels=n_channels, input_size=input_size, n_classes=n_classes, kernel_size=8
+    method = DeepCoral(
+        ToyModule(),
+        reg=1,
+        layer_name="dropout",
+        batch_size=10,
+        max_epochs=10,
+        train_split=None
     )
-    module.eval()
 
-    rng = torch.random.manual_seed(42)
-    n_samples = 20
-    X = torch.randn(size=(n_samples, n_channels, input_size), generator=rng)
-    y = torch.randint(high=n_classes, size=(n_samples,), generator=rng)
-    X_target = torch.randn(size=(n_samples, n_channels, input_size), generator=rng)
+    X, y, sample_domain = dataset.pack_train(as_sources=["s"], as_targets=["t"])
+    method.fit(X.astype(np.float32), y, sample_domain)
 
-    method = DAN(
-        module=module,
-        criterion=nn.CrossEntropyLoss(),
-        layer_names=["feature_extractor"],
-        max_epochs=2,
-    )
-    method.fit(X, y, X_target=X_target)
-    y_pred = method.predict(X_target)
+    X_test, y_test, sample_domain_test = dataset.pack_test(as_targets=["t"])
 
-    assert y_pred.shape[0] == n_samples
+    y_pred = method.predict(X_test.astype(np.float32), sample_domain_test)
+
+    assert y_pred.shape[0] == X_test.shape[0]
+
+    history = method.history_
+
+    assert history[0]["train_loss"] > history[-1]["train_loss"]
