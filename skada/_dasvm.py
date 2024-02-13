9c6fcadb2eff4516e719f1d03524b2aba00b228e
Rouxben <61913555+Rouxben@users.noreply.github.com>
2024-02-13 10:49:35 +0100

    changed some variable names
    used only lowecarse for variable names

--- skada/_dasvm.py
+++ skada/_dasvm.py
@@ -46,39 +46,39 @@ class DASVMEstimator(BaseEstimator):
         self.Stop = Stop
         self.k = k
 
-    def _find_points_next_step(self, Indices_list, d):
+    def _find_points_next_step(self, indices_list, d):
         """
         This function allow us to find the next points to add/discard,
-        It is an inplace method, changing Indices_list
+        It is an inplace method, changing indices_list
         """
         # We should take k points for each of the c classes,
         # depending on the values of d
-        for j in range(min(self.k, math.ceil(sum(~Indices_list)/self.c))):
-            I = np.unique(np.argmax(d[~Indices_list], axis=0))
+        for j in range(min(self.k, math.ceil(sum(~indices_list)/self.c))):
+            I = np.unique(np.argmax(d[~indices_list], axis=0))
             # We need to get all those indices to be take into account
             # the fact that the some previous points weren't in the list
-            for l in range(len(Indices_list)):
-                if Indices_list[l]:
+            for l in range(len(indices_list)):
+                if indices_list[l]:
                     I[I >= l] += 1
 
             # We finally only need to change the list
             for l in I:
-                # Indices_list[l] is False at that point
-                Indices_list[l] = True
+                # indices_list[l] is False at that point
+                indices_list[l] = True
 
     def _get_X_y(
-            self, new_estimator, Index_target_added, Index_source_deleted, Xs, Xt, ys
+            self, new_estimator, index_target_added, index_source_deleted, Xs, Xt, ys
             ):
         # Allow to get the X and y arrays at a state of the algorithm
         # We take the source datapoints that have not been
         # deleted, and the target points
         # that have been added
-        X = np.concatenate((Xs[~Index_source_deleted], Xt[Index_target_added]))
-        y = np.concatenate((ys[~Index_source_deleted], new_estimator.predict(
-            Xt[Index_target_added])))
+        X = np.concatenate((Xs[~index_source_deleted], Xt[index_target_added]))
+        y = np.concatenate((ys[~index_source_deleted], new_estimator.predict(
+            Xt[index_target_added])))
         return X, y
 
-    def get_decision(self, new_estimator, X, Indices_list):
+    def get_decision(self, new_estimator, X, indices_list):
         # We look at the points that have either not been discarded or not been added
         # We are assuming that the `decision_function` from the base_estimator is:
         # giving c values between -1 and c-1, not having the same
@@ -86,17 +86,17 @@ class DASVMEstimator(BaseEstimator):
         # When c == 2, we assume we only get one value (as for SVC)
         # We take c to be the number of classes on which `new_estimator`
         # has been fitted
-        if sum(~Indices_list) > 0:
-            df = new_estimator.decision_function(X[~Indices_list])
+        if sum(~indices_list) > 0:
+            df = new_estimator.decision_function(X[~indices_list])
             # df.ndim allows us to know if we are in the
             # `binary` case or the `multiclass` one
             if df.ndim == 1:
                 decisions = np.ones(X.shape[0])
-                decisions[~Indices_list] = df
+                decisions[~indices_list] = df
                 decisions = np.array([-decisions, decisions]).T
             else:
                 decisions = np.ones((X.shape[0], df.shape[1]))
-                decisions[~Indices_list] = df
+                decisions[~indices_list] = df
         else:
             decisions = np.ones(X.shape[0])
         return decisions
@@ -129,10 +129,10 @@ class DASVMEstimator(BaseEstimator):
 
         # This is the list of the indices from the
         # points from Xs that have been discarded
-        Index_source_deleted = np.array([False]*n)
+        index_source_deleted = np.array([False]*n)
         # This is the list of the indices from the
         # points from Xt that have been added
-        Index_target_added = np.array([False]*m)
+        index_target_added = np.array([False]*m)
 
         # In the first step, the SVC is fitted on the source
         X = Xs
@@ -158,35 +158,35 @@ class DASVMEstimator(BaseEstimator):
 
         # doing the selection on the labeled data
         self._find_points_next_step(
-            Index_source_deleted, decisions_s)
+            index_source_deleted, decisions_s)
         # doing the selection on the semi-labeled data
         self._find_points_next_step(
-            Index_target_added, decisions_ta)
+            index_target_added, decisions_ta)
 
         i = 0
         while (
-                sum(Index_target_added) < m or sum(Index_source_deleted) < n
+                sum(index_target_added) < m or sum(index_source_deleted) < n
                 ) and i < self.Stop:
             i += 1
             old_estimator = new_estimator
             X, y = self._get_X_y(
-                new_estimator, Index_target_added, Index_source_deleted, Xs, Xt, ys)
+                new_estimator, index_target_added, index_source_deleted, Xs, Xt, ys)
 
             new_estimator = clone(self.base_estimator)
             new_estimator.fit(X, y)
 
-            for j in range(len(Index_target_added)):
-                if Index_target_added[j]:
+            for j in range(len(index_target_added)):
+                if index_target_added[j]:
                     x = Xt[j]
                     if new_estimator.predict(
                             [x]) != old_estimator.predict([x]):
-                        # Index_target_added[j] should be True
-                        Index_target_added[j] = False
+                        # index_target_added[j] should be True
+                        index_target_added[j] = False
 
             # look at those that have not been discarded
-            decisions_s = self.get_decision(new_estimator, Xs, Index_source_deleted)
+            decisions_s = self.get_decision(new_estimator, Xs, index_source_deleted)
             # look at those that haven't been added
-            decisions_ta = self.get_decision(new_estimator, Xt, Index_target_added)
+            decisions_ta = self.get_decision(new_estimator, Xt, index_target_added)
 
             # We want to take values the estimator is unsure about, meaning that we
             # want those that have values the closest that we can to c-1
@@ -194,9 +194,9 @@ class DASVMEstimator(BaseEstimator):
             decisions_ta = -np.abs(decisions_ta-self.c-1)
 
             # doing the selection on the labeled data
-            self._find_points_next_step(Index_source_deleted, decisions_s)
+            self._find_points_next_step(index_source_deleted, decisions_s)
             # doing the selection on the semi-labeled data
-            self._find_points_next_step(Index_target_added, decisions_ta)
+            self._find_points_next_step(index_target_added, decisions_ta)
 
         old_estimator = new_estimator
         # On last fit only on semi-labeled data
@@ -207,7 +207,7 @@ class DASVMEstimator(BaseEstimator):
 
         self.base_estimator_ = new_estimator
 
-        # or the list of Index_target_added and Index_source_deleted (this making
+        # or the list of index_target_added and index_source_deleted (this making
         # them being an attribute if the object)
         return self
 
