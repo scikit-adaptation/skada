36a3c4eac480aae3d1734521b8594f94ced08b43
tgnassou <theo.gnassounou@gmail.com>
2024-02-13 10:14:20 +0100

    change name of example

--- examples/methods/plot_deepcoral.py
+++ examples/methods/plot_deepcoral.py
@@ -1,6 +1,7 @@
 # %%
 from skada.datasets import make_shifted_datasets
-from skada.feature import DeepCoral, ToyModule
+from skada.feature import DeepCoral, DANN, CDAN, DeepJDOT
+from skada.feature import ToyModule
 import numpy as np
 
 # %%
@@ -9,7 +10,7 @@ import numpy as np
 n_samples = 20
 dataset = make_shifted_datasets(
     n_samples_source=n_samples,
-    n_samples_target=n_samples + 1,
+    n_samples_target=n_samples,
     shift="concept_drift",
     noise=0.1,
     random_state=42,
@@ -19,8 +20,8 @@ X, y, sample_domain = dataset.pack_train(as_sources=["s"], as_targets=["t"])
 
 # %%
 # create skorch model
-net = DeepCoral(ToyModule(), reg=1)
-
+net = DeepCoral(ToyModule(), reg=1, layer_name="dropout", batch_size=10, train_split=None)
+net = DeepJDOT(ToyModule(), layer_name="dropout", batch_size=10, train_split=None)
 # %%
 # create a dict of X and sample_domain
 X_dict = {"X": X.astype(np.float32), "sample_domain": sample_domain}
@@ -31,4 +32,8 @@ net.fit(
 
 X_test, y_test, sample_domain_test = dataset.pack_test(as_targets=["t"])
 X_dict_test = {"X": X_test.astype(np.float32), "sample_domain": sample_domain_test}
+
+# %%
 net.score(X_dict_test, y_test)
+
+# %%
