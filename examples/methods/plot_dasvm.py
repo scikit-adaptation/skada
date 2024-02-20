1c2aa7e3fec56c07181e204f3c530fa3f1f71575
Rouxben <61913555+Rouxben@users.noreply.github.com>
2024-02-20 13:28:38 +0100

    made some changes to the example

--- examples/methods/plot_dasvm.py
+++ examples/methods/plot_dasvm.py
@@ -25,14 +25,16 @@ from sklearn.svm import SVC
 
 # base_estimator can be any classifier equipped with `decision_function` such as:
 # SVC(gamma='auto'), LogisticRegression(random_state=0), etc...
-base_estimator = SVC(kernel="Linear")
+base_estimator = SVC(kernel="linear")
+target_marker = "x"
+source_marker = "+"
 
 xlim = (-2.2, 2.5)
-ylim = (-1.5, 2)
+ylim = (-1.5, 1.5)
 
 figure, axis = plt.subplots(1, 2)
 
-N = 100
+N = 50
 theta_s = np.concatenate((
     np.linspace(10, 170, N),
     np.linspace(190, 350, N),
@@ -48,10 +50,12 @@ Xs = np.array([
     (1+np.random.normal(0, 0.1, theta_s.shape[0]))*np.cos(theta_s),
     (1+np.random.normal(0, 0.1, theta_s.shape[0]))*np.sin(theta_s)
     ]).T
+Xs += np.array([[-0.5, 0]]*N+[[0.5, 0]]*N)
 Xt = np.array([
     (1+np.random.normal(0, 0.1, theta_t.shape[0]))*np.cos(theta_t),
     (1+np.random.normal(0, 0.1, theta_t.shape[0]))*np.sin(theta_t)
     ]).T
+Xt += np.array([[-0.5, 0]]*N+[[0.5, 0]]*N)
 X = np.concatenate((Xs, Xt))
 
 ys = np.array([0]*N+[1]*N)
@@ -60,12 +64,12 @@ y = np.concatenate((ys, yt))
 
 sample_domain = np.array([1]*2*N+[-2]*2*N)
 
-axis[0].scatter(Xs[:, 0], Xs[:, 1], c=ys)
+axis[0].scatter(Xs[:, 0], Xs[:, 1], c=ys, marker=source_marker)
 axis[0].set_xlim(xlim)
 axis[0].set_ylim(ylim)
 axis[0].set_title("source data points")
 
-axis[1].scatter(Xt[:, 0], Xt[:, 1], c=yt)
+axis[1].scatter(Xt[:, 0], Xt[:, 1], c=yt, marker=target_marker)
 axis[1].set_xlim(xlim)
 axis[1].set_ylim(ylim)
 axis[1].set_title("target data points")
@@ -73,12 +77,12 @@ axis[1].set_title("target data points")
 figure.suptitle("data points", fontsize=20)
 
 estimator = DASVMEstimator(
-    base_estimator=clone(base_estimator), k=10,
+    base_estimator=clone(base_estimator), k=5,
     save_estimators=True, save_indices=True).fit(
     X, y, sample_domain=sample_domain)
 
-epsilon = 0.05
-N = 4
+epsilon = 0.02
+N = 5
 K = len(estimator.estimators)//N
 figure, axis = plt.subplots(1, N+1)
 for i in list(range(0, N*K, K)) + [-1]:
@@ -104,24 +108,32 @@ for i in list(range(0, N*K, K)) + [-1]:
         X_[:, 0], X_[:, 1], c=[1]*X_.shape[0],
         alpha=1, cmap="autumn", s=[0.1]*X_.shape[0])
 
+    X_s = Xs[~estimator.indices_source_deleted[i]]
+    X_t = Xt[estimator.indices_target_added[i]]
     X = np.concatenate((
-        Xs[~estimator.indices_source_deleted[i]],
-        Xt[estimator.indices_target_added[i]]))
+        X_s,
+        X_t))
 
     if sum(estimator.indices_target_added[i]) > 0:
         semi_labels = e.predict(Xt[estimator.indices_target_added[i]])
+        axis[j].scatter(
+            X_s[:, 0], X_s[:, 1], c=ys[~estimator.indices_source_deleted[i]],
+            marker=source_marker)
+        a = axis[j].scatter(
+            X_t[:, 0], X_t[:, 1], c=semi_labels,
+            marker=target_marker)
     else:
         semi_labels = np.array([])
-    a = axis[j].scatter(X[:, 0], X[:, 1], c=np.concatenate((
-        ys[~estimator.indices_source_deleted[i]],
-        semi_labels)))
+        a = axis[j].scatter(
+            X[:, 0], X[:, 1], c=ys[~estimator.indices_source_deleted[i]])
     X = Xt[~estimator.indices_target_added[i]]
     axis[j].scatter(
         X[:, 0], X[:, 1], cmap="gray",
-        c=[0.5]*X.shape[0], alpha=0.5, vmax=1, vmin=0)
+        c=[0.5]*X.shape[0], alpha=0.5, vmax=1, vmin=0,
+        marker=target_marker)
 
     axis[j].set_xlim(xlim)
     axis[j].set_ylim(ylim)
 figure.colorbar(a)
-figure.suptitle("reasulting predictions", fontsize=20)
+figure.suptitle("evolutions of predictions", fontsize=20)
 plt.show()
