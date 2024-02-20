c94d1038784165c952c26b157c1dc4ae5a923af8
Yanis Lalou <ylalou.g@gmail.com>
2024-02-20 16:36:18 +0100

    remove typo error

--- skada/model_selection.py
+++ skada/model_selection.py
@@ -343,7 +343,7 @@ class StratifiedDomainShuffleSplit(BaseDomainAwareShuffleSplit):
     >>> from skada.model_selection import StratifiedDomainShuffleSplit
     >>> X = np.ones((10, 2))
     >>> y = np.array([-1, 0, 1, -1, 0, 1, -1, 0, 1, -1])
-    >>> sample_domain = np.array([-2, 1, 1, -2s, 1, 1, -2, 1, 1, -2])
+    >>> sample_domain = np.array([-2, 1, 1, -2, 1, 1, -2, 1, 1, -2])
     >>> da_shufflesplit = StratifiedDomainShuffleSplit(n_splits=2,
     ... random_state=0, test_size=0.5)
     >>> da_shufflesplit.get_n_splits(X, y, sample_domain)
