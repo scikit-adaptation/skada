60e67dcc9c8711bcde67d24f6d08139880d7df82
Rouxben <61913555+Rouxben@users.noreply.github.com>
2024-02-26 09:07:56 +0100

    made the exampe code clearer and set a random seed now

--- examples/plot_1nn_reweight.py
+++ examples/plot_1nn_reweight.py
@@ -24,11 +24,9 @@ from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.linear_model import LogisticRegression
 from skada._reweight import NearestNeighborReweightDensity
 
-from skada.datasets import make_shifted_datasets
-
 # Use same random seed for multiple calls to make_datasets to
 # ensure same distributions
-RANDOM_SEED = 42
+RANDOM_SEED = 1
 
 names = [
     "Without da",
@@ -42,22 +40,8 @@ classifiers = [
         laplace_smoothing=True),
 ]
 
-ns = 10
-nt = 10
-
-datasets = [
-    make_shifted_datasets(
-        n_samples_source=ns,
-        n_samples_target=nt,
-        shift="covariate_shift",
-        label="binary",
-        noise=0.4,
-        random_state=RANDOM_SEED,
-        return_dataset=True
-    ),
-]
-
-# We create the dataset
+np.random.seed(RANDOM_SEED)
+# We create the dataset, 2*N is the sample size of source and target
 N = 20
 Xs = np.array(
     [[1+np.random.normal(0, 0.4), 1+np.random.normal(0, 0.4)] for i in range(N)] +
@@ -84,34 +68,66 @@ y_min, y_max = -1.5, 1.5
 
 figure, axes = plt.subplots(len(classifiers) + 1, 2, figsize=(7, 21))
 # iterate over datasets
-for ds_cnt, ds in enumerate(datasets):
-    # just plot the dataset first
-    cm = plt.cm.RdBu
-    cm_bright = ListedColormap(["#FF0000", "#0000FF"])
-    ax = axes[0, 1]
-    if ds_cnt == 0:
-        ax.set_ylabel("Source data")
-    # Plot the source points
-    ax.scatter(
-        Xs[:, 0],
-        Xs[:, 1],
-        c=ys,
-        cmap=cm_bright,
-        alpha=0.5,
+    
+# just plot the dataset first
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(["#FF0000", "#0000FF"])
+ax = axes[0, 1]
+ax.set_ylabel("Source data")
+# Plot the source points
+ax.scatter(
+    Xs[:, 0],
+    Xs[:, 1],
+    c=ys,
+    cmap=cm_bright,
+    alpha=0.5,
+)
+
+ax = axes[0, 0]
+
+ax.set_ylabel("Target data")
+# Plot the target points
+ax.scatter(
+    Xt[:, 0],
+    Xt[:, 1],
+    c=ys,
+    cmap=cm_bright,
+    alpha=0.1,
+)
+ax.scatter(
+    Xt[:, 0],
+    Xt[:, 1],
+    c=yt,
+    cmap=cm_bright,
+    alpha=0.5,
+)
+ax.set_xlim(x_min, x_max)
+ax.set_ylim(y_min, y_max)
+ax.set_xticks(())
+ax.set_yticks(())
+i = 1
+
+# iterate over classifiers
+for name, clf in zip(names, classifiers):
+    print(name, clf)
+    ax = axes[i, 0]
+    if name == "Without da":
+        clf.fit(Xs, ys)
+    else:
+        clf.fit(X, y, sample_domain=sample_domain)
+    score = clf.score(Xt, yt)
+    DecisionBoundaryDisplay.from_estimator(
+        clf, Xs, cmap=cm, alpha=0.8, ax=ax, eps=0.5, response_method="predict",
     )
 
-    ax = axes[0, 0]
+    if name == "1NN Reweight Density":
+        size = 10*clf.named_steps[
+            'nearestneighbordensityadapter'].base_estimator.get_weights(
+                Xs, Xt)
+    else:
+        size = np.array([30]*Xs.shape[0])
 
-    if ds_cnt == 0:
-        ax.set_ylabel("Target data")
     # Plot the target points
-    ax.scatter(
-        Xt[:, 0],
-        Xt[:, 1],
-        c=ys,
-        cmap=cm_bright,
-        alpha=0.1,
-    )
     ax.scatter(
         Xt[:, 0],
         Xt[:, 1],
@@ -119,75 +135,39 @@ for ds_cnt, ds in enumerate(datasets):
         cmap=cm_bright,
         alpha=0.5,
     )
+
     ax.set_xlim(x_min, x_max)
-    ax.set_ylim(y_min, y_max)
+
     ax.set_xticks(())
     ax.set_yticks(())
-    i = 1
+    ax.set_ylabel(name)
+    ax.text(
+        x_max - 0.3,
+        y_min + 0.3,
+        ("%.2f" % score).lstrip("0"),
+        size=15,
+        horizontalalignment="right",
+    )
 
-    # iterate over classifiers
-    for name, clf in zip(names, classifiers):
-        print(name, clf)
-        ax = axes[i, ds_cnt]
-        if name == "Without da":
-            clf.fit(Xs, ys)
-        else:
-            clf.fit(X, y, sample_domain=sample_domain)
-        score = clf.score(Xt, yt)
-        DecisionBoundaryDisplay.from_estimator(
-            clf, Xs, cmap=cm, alpha=0.8, ax=ax, eps=0.5, response_method="predict",
-        )
+    ax = axes[i, 1]
 
-        if name == "1NN Reweight Density":
-            size = 10*clf.named_steps[
-                'nearestneighbordensityadapter'].base_estimator.get_weights(
-                    Xs, Xt)
-        else:
-            size = np.array([30]*Xs.shape[0])
+    # Plot the target points
+    ax.scatter(
+        Xs[:, 0],
+        Xs[:, 1],
+        c=ys,
+        cmap=cm_bright,
+        alpha=0.5,
+        s=size
+    )
 
-        # Plot the target points
-        ax.scatter(
-            Xt[:, 0],
-            Xt[:, 1],
-            c=yt,
-            cmap=cm_bright,
-            alpha=0.5,
-        )
+    ax.set_xlim(x_min, x_max)
 
-        ax.set_xlim(x_min, x_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_ylabel("obtained weights")
 
-        ax.set_xticks(())
-        ax.set_yticks(())
-        if ds_cnt == 0:
-            ax.set_ylabel(name)
-        else:
-            ax.set_ylabel("obtained weights")
-        ax.text(
-            x_max - 0.3,
-            y_min + 0.3,
-            ("%.2f" % score).lstrip("0"),
-            size=15,
-            horizontalalignment="right",
-        )
-
-        ax = axes[i, 1]
-
-        # Plot the target points
-        ax.scatter(
-            Xs[:, 0],
-            Xs[:, 1],
-            c=ys,
-            cmap=cm_bright,
-            alpha=0.5,
-            s=size
-        )
-
-        ax.set_xlim(x_min, x_max)
-
-        ax.set_xticks(())
-        ax.set_yticks(())
-
-        i += 1
+    i += 1
 
 figure.suptitle("Comparison of the weighting da methods")
 plt.tight_layout()
