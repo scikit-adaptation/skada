2b68f096dd7e983e765b428e47489bd8c8c7363a
Rémi Flamary <remi.flamary@gmail.com>
2024-02-29 09:28:55 +0100

    [MRG] JDOTClassifier + doc update (#113)
    * update doc with howto in menu and chnage quickstart to Users guide
    
    * add jdot classifier and exmaple
    
    * pep8
    
    * debug method comparison + handle weighst morr gracefully
    
    * add tests fr jdot functions
    
    * comments theo
    
    * proper ref in exmaple
    
    * f__ whitespace

--- skada/__init__.py
+++ skada/__init__.py
@@ -40,8 +40,12 @@ from ._subspace import (
     TransferComponentAnalysisAdapter,
     TransferComponentAnalysis,
 )
+from ._ot import (
+    solve_jdot_regression,
+    JDOTRegressor,
+    solve_jdot_classification,
+    JDOTClassifier)
 from ._self_labeling import DASVMClassifier
-from ._ot import solve_jdot_regression, JDOTRegressor
 from ._pipeline import make_da_pipeline
 from .utils import source_target_split
 
@@ -88,6 +92,8 @@ __all__ = [
     "DASVMClassifier",
     "solve_jdot_regression",
     "JDOTRegressor",
+    "solve_jdot_classification",
+    "JDOTClassifier",
 
     "make_da_pipeline",
 
