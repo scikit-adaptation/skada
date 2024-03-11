c3db5f57f04d6488c519570953579b4b63d04610
Oleksii Kachaiev <kachayev@gmail.com>
2024-03-11 17:02:07 +0100

    Fix lint for __inits__

--- skada/deep/__init__.py
+++ skada/deep/__init__.py
@@ -1,37 +1,35 @@
 # Author: Theo Gnassounou <theo.gnassounou@inria.fr>
 #         Remi Flamary <remi.flamary@polytechnique.edu>
+#         Oleksii Kachaiev <kachayev@gmail.com>
 #
 # License: BSD 3-Clause
 
-"""
-Some methods for deep domain adaptation.
-"""
+"""Some methods for deep domain adaptation."""
+
 try:
-    import torch  # noqa: F401
     import skorch  # noqa: F401
+    import torch  # noqa: F401
 except (ImportError, ModuleNotFoundError) as e:
     raise ImportError(
         "torch and skorch are required for importing skada.deep.* modules."
     ) from e
 
-from ._divergence import DeepCoral, DeepCoralLoss, DANLoss, DAN
+from . import losses, modules
+from ._adversarial import CDAN, DANN, CDANLoss, DANNLoss
+from ._divergence import DAN, DANLoss, DeepCoral, DeepCoralLoss
 from ._optimal_transport import DeepJDOT, DeepJDOTLoss
-from ._adversarial import DANN, CDAN, DANNLoss, CDANLoss
-
-from . import losses
-from . import modules
 
 __all__ = [
-    'losses',
-    'modules',
-    'DeepCoralLoss',
-    'DeepCoral',
-    'DANLoss',
-    'DAN',
-    'DeepJDOTLoss',
-    'DeepJDOT',
-    'DANNLoss',
-    'DANN',
-    'CDANLoss',
-    'CDAN',
+    "losses",
+    "modules",
+    "DeepCoralLoss",
+    "DeepCoral",
+    "DANLoss",
+    "DAN",
+    "DeepJDOTLoss",
+    "DeepJDOT",
+    "DANNLoss",
+    "DANN",
+    "CDANLoss",
+    "CDAN",
 ]
