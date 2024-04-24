9531a4ade2399fb641a9f844fbb112a392f8b133
Yanis Lalou <ylalou.g@gmail.com>
2024-04-24 15:15:17 +0200

    For Circ Val, allow any estimator supproting clone or deepcopy

--- skada/metrics.py
+++ skada/metrics.py
@@ -552,21 +552,17 @@ class CircularValidation(_BaseDomainAwareScorer):
         X, y, sample_domain = check_X_y_domain(X, y, sample_domain)
         source_idx = extract_source_indices(sample_domain)
 
-        # if estimator is NeuralNet
-        if not isinstance(estimator, BaseEstimator):
-            # The estimator is a deep model
-            from skorch import NeuralNet
-
-            if isinstance(estimator, NeuralNet):
-                backward_estimator = clone(estimator)
-            else:
-                raise ValueError(
-                    "The estimator passed should be a scikit-learn estimator or "
-                    "a skorch.NeuralNet "
-                    f"The estimator {estimator!r} is not."
-                )
-        else:
+        try:
             backward_estimator = deepcopy(estimator)
+        except (TypeError, AttributeError):
+            try:
+                backward_estimator = clone(estimator)
+            except Exception:
+                raise ValueError(
+                    "The estimator passed should be a scikit-learn estimator, "
+                    "a skorch.NeuralNet, or an object that supports deepcopy or clone. "
+                    f"The estimator {estimator!r} does not meet these requirements."
+                )
 
         y_pred_target = estimator.predict(
             X[~source_idx], sample_domain=sample_domain[~source_idx]
