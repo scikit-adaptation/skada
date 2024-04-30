ac67704d37b999e141fd563573bc2af340c910bf
Ambroise Odonnat <ambroiseodonnattechnologie@gmail.com>
2024-04-30 17:50:58 +0200

    Update __init__.py

--- skada/datasets/__init__.py
+++ skada/datasets/__init__.py
@@ -29,19 +29,26 @@ from ._samples_generator import (
 )
 from ._mnist_usps import load_mnist_usps
 
+from ._office_home import (
+    fetch_office_home_resnet50,
+    fetch_domain_aware_office_home_resnet50,
+)
+
 __all__ = [
-    'DomainAwareDataset',
-    'Office31CategoriesPreset',
-    'Office31Domain',
-    'get_data_home',
-    'fetch_office31_decaf',
-    'fetch_office31_decaf_all',
-    'fetch_office31_surf',
-    'fetch_office31_surf_all',
-    'make_shifted_blobs',
-    'make_shifted_datasets',
-    'make_dataset_from_moons_distribution',
-    'make_variable_frequency_dataset',
-    'select_domain',
-    'load_mnist_usps',
+    "DomainAwareDataset",
+    "Office31CategoriesPreset",
+    "Office31Domain",
+    "get_data_home",
+    "fetch_office31_decaf",
+    "fetch_office31_decaf_all",
+    "fetch_office31_surf",
+    "fetch_office31_surf_all",
+    "make_shifted_blobs",
+    "make_shifted_datasets",
+    "make_dataset_from_moons_distribution",
+    "make_variable_frequency_dataset",
+    "select_domain",
+    "load_mnist_usps",
+    "fetch_office_home_resnet50",
+    "fetch_domain_aware_office_home_resnet50",
 ]
