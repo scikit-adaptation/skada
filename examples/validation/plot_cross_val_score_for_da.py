e2db5178a48b15bb56c611cb88979d8d1843c9c0
Apolline Mellot <81578853+apmellot@users.noreply.github.com>
2024-07-04 14:21:41 +0200

    [FIX][ENH] Fix and add more explanation to "DA validation procedures" examples (#183)
    * update example
    
    * text
    
    * lint
    
    * oversight
    
    * fix ranfom_seed
    
    * more details in grid search ex
    
    * improve text
    
    * improve text bis
    
    * update explanation
    
    * ruff format
    
    * improve text cv plot
    
    * typo
    
    * ruff format
    
    * change skada._pipeline to skada
    
    ---------
    
    Co-authored-by: Rémi Flamary <remi.flamary@gmail.com>
    Co-authored-by: Théo Gnassounou <66993815+tgnassou@users.noreply.github.com>
    Co-authored-by: tgnassou <theo.gnassounou@gmail.com>

--- examples/validation/plot_cross_val_score_for_da.py
+++ examples/validation/plot_cross_val_score_for_da.py
@@ -2,18 +2,20 @@
 Using cross_val_score with skada
 ================================
 
-This illustrates the use of DA scorer such :class:`~skada.metrics.TargetAccuracyScorer`
+This example illustrates the use of DA scorer such as :class:`~skada.metrics.TargetAccuracyScorer`
 with `cross_val_score <https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_val_score.html#sklearn.model_selection.cross_val_score>`_.
 """  # noqa
 # %%
-# Prepare dataset and estimators
+# We first create a shifted dataset. Then we prepare the pipeline including a
+# base estimator doing the classification and the DA estimator. We use
+# :code:`ShuffleSplit` as cross-validation strategy.
 
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import ShuffleSplit, cross_val_score
 from sklearn.svm import SVC
 
-from skada import EntropicOTMapping, source_target_split
+from skada import EntropicOTMapping, make_da_pipeline, source_target_split
 from skada.datasets import make_shifted_datasets
 from skada.metrics import SupervisedScorer
 
@@ -35,13 +37,16 @@ X, y, sample_domain = dataset.pack_train(as_sources=["s"], as_targets=["t"])
 X_source, X_target, y_source, y_target = source_target_split(
     X, y, sample_domain=sample_domain
 )
-cv = ShuffleSplit(n_splits=5, test_size=0.3, random_state=0)
+cv = ShuffleSplit(n_splits=5, test_size=0.3, random_state=RANDOM_SEED)
 
 # %%
-# Cross Validate using supervised labels from the target domain
-#
-# Supervised scoring requires target labels to be passed into the pipeline
-# separately, so they are only available for the scoring.
+# The DA estimator pipeline is ready to be used with :code:`cross_val_score`.
+# Source data from the training splits is first adapted with the target
+# data from the same splits and then used to fit the base estimator.
+# The target data from the test split is used to compute the score.
+# The separation between source and target data is done automatically
+# by the DA pipeline thanks to :code:`sample_domain`. The :code:`target_labels`
+# are only used by the :code:`SupervisedScorer`.
 
 _, target_labels, _ = dataset.pack(as_sources=["s"], as_targets=["t"], train=False)
 scores_sup = cross_val_score(
@@ -59,19 +64,22 @@ print(
 )
 
 # %%
-# Compare scores with the simple estimator with no adaptation
+# To evaluate the performance of the DA estimator, we compare it with the
+# performance of the base estimator without DA. We use the same cross-validation
+# strategy and the same data splits. We create a DA pipeline with
+# :code:`make_da_pipeline` including the base estimator only. The
+# :code:`sample_domain` and :code:`target_labels` are also passed to the pipeline
+# to separate the source and target data and to compute the score.
 
-
-def _scorer(estimator, X, y):
-    return estimator.score(X_target, y_target)
-
+estimator_no_da = make_da_pipeline(base_estimator)
 
 scores_no_da = cross_val_score(
-    base_estimator,
-    X_source,
-    y_source,
+    estimator_no_da,
+    X,
+    y,
     cv=cv,
-    scoring=_scorer,
+    params={"sample_domain": sample_domain, "target_labels": target_labels},
+    scoring=SupervisedScorer(),
 )
 
 print(
