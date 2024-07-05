9243ffee04e6cb72c0e35355fe7ac966c48ac23d
Rémi Flamary <remi.flamary@gmail.com>
2024-07-05 11:49:04 +0200

    ste minimal setup.py for legacy pip

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
 from setuptools import find_packages, setup
 
-setup(
-    name='skada',
-    version='0.3',
-    description='A domain adaptation toolbox to reduce shift between domains.',
-
-    # The project's main homepage.
-    url='https://github.com/scikit-adaptation/skada',
-
-    # Author details
-    author='Théo Gnassounou',
-    author_email='theo.gnassounou@inria.fr',
-
-    # Choose your license
-    license='BSD 3-Clause',
-    # What does your project relate to?
-    keywords='domain adaptation, machine learning, deep-learning',
-
-    packages=find_packages(),
-)
+setup()
