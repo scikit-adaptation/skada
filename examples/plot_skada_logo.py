296f17976ffee83ad11aac080743ef31915590de
tgnassou <theo.gnassounou@gmail.com>
2024-08-29 10:54:55 +0200

    add new logo

--- examples/plot_skada_logo.py
+++ examples/plot_skada_logo.py
@@ -10,315 +10,416 @@ This logo is that it is done 100% in Python and generated using
 matplotlib and plotting the solution of the EMD solver from POT.
 """
 
-# Author: Remi Flamary
+# Author: Théo Gnassounou
 #
 # License: BSD 3-Clause
 # sphinx_gallery_thumbnail_number = 1
-# %% Imports
+
+# %%
+from math import factorial
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pylab as pl
 
 plt.rcParams.update({"text.usetex": True, "font.family": "Computer Modern"})
 
-
-def get_lda_interp(xs, xt, alpha, len_=1):
-    """Compute the LDA interpolation between two domains"""
-    # compute means (classes assumed to be balanced)
-    m1 = (1 - alpha) * xs[: n // 2, :].mean(0) + (alpha) * xt[: n // 2, :].mean(0)
-    m2 = (1 - alpha) * xs[n // 2 :, :].mean(0) + (alpha) * xt[n // 2 :, :].mean(0)
-
-    vo = m2 - m1
-    vo = vo / np.linalg.norm(vo)
-
-    theta = np.pi / 2
-    rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
-
-    vor = rot.dot(vo)
-
-    mm = 0.5 * m1 + 0.5 * m2
-
-    return np.vstack((mm - len_ * vor, mm + len_ * vor))
+color_S = "black"
+color_1 = "#2364aa"
+color_0 = "#c84630"
+color_unlabel = "#999c81"
 
 
-# %% Generate Data
+def comb(n, k):
+    return factorial(n) // (factorial(k) * factorial(n - k))
 
 
-n = 40
-
-np.random.seed(42)
-
-xs = 0.2 * np.random.randn(n, 2)
-
-# class  specific change
-xs[n // 2 :, 0] += 0.5
-xs[n // 2 :, 1] -= 1
-
-# classes 0 an 3 for blue/red colors
-ys = np.zeros(n)
-ys[n // 2 :] = 3
-
-# global changes
-xt = 0.15 * np.random.randn(n, 2)
-xt[:, 0] += 0.7
-xt[:, 1] -= 0
-
-# class specific change
-xt[n // 2 :, 0] += 1
-xt[n // 2 :, 1] -= 0.2
-
-# class 7 for gray color (target without label)
-yt = np.ones(n) * 7
+def bezier_curve(t, points):
+    n = len(points) - 1
+    return sum(comb(n, i) * (1 - t) ** (n - i) * t**i * points[i] for i in range(n + 1))
 
 
-# %%
-# Plot the small logo
-# ===================
+rng = np.random.RandomState(42)
 
+# Create a figure and axis
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.set_xlim(-1, 11)
+ax.set_ylim(-2, 11.2)
 
-nb = 10
-alpha0 = 0.2
+# Define the control points for the Bézier curve
+points = np.array([[2, 8], [4, 10], [6, 0], [8, 2]])
 
-alphalist = np.linspace(0, 1, nb)
+# Generate the S shape
+t_values = np.linspace(-0.3, 1.3, 100)
+curve = np.array([bezier_curve(t, points) for t in t_values])
 
-pl.figure(1, (2, 2))
-pl.clf()
-alpha = 0.7
-
-# plot samples
-pl.scatter(
-    xs[ys == 0, 0],
-    xs[ys == 0, 1],
-    c="C0",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="o",
-)
-pl.scatter(
-    xs[ys == 3, 0],
-    xs[ys == 3, 1],
-    c="C3",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="s",
-)
-pl.scatter(
-    xt[ys == 0, 0],
-    xt[ys == 0, 1],
-    c="C7",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="o",
-)
-pl.scatter(
-    xt[ys == 3, 0],
-    xt[ys == 3, 1],
-    c="C7",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="s",
-)
-
-# plot classifiers
-ax = pl.axis()
-for i in range(nb):
-    xclass = get_lda_interp(xs, xt, alphalist[i], 2)
-    pl.plot(
-        xclass[:, 0],
-        xclass[:, 1],
-        color="C2",
-        alpha=alpha0 + (1 - alpha0) * alphalist[i],
-        zorder=0,
-        linewidth=2,
+start_length = 10
+for i, angle in enumerate(range(0, 50, 10)):
+    rotation_center = np.mean(curve, axis=0)
+    rotation_angle = np.radians(angle)
+    rotation_matrix = np.array(
+        [
+            [np.cos(rotation_angle), -np.sin(rotation_angle)],
+            [np.sin(rotation_angle), np.cos(rotation_angle)],
+        ]
     )
-pl.axis(ax)
-pl.axis("off")
+    rotated_curve = np.dot(curve - rotation_center, rotation_matrix) + rotation_center
 
-# save to file
-pl.savefig("skada_logo.svg", bbox_inches="tight", dpi=400)
-pl.savefig("skada_logo.png", bbox_inches="tight", dpi=400)
-
-
-# %%
-# Plot the full logo
-# ==================
-
-# target data for full logo
-xt2 = xt.copy()
-xt2[:, 0] += 6.3
-
-
-nb = 10
-alpha0 = 0.2
-alphalist = np.linspace(0, 1, nb)
-
-pl.figure(2, (9, 1.5))
-pl.clf()
-alpha = 0.7
-
-# plot samples
-pl.scatter(
-    xs[ys == 0, 0],
-    xs[ys == 0, 1],
-    c="C0",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="o",
-)
-pl.scatter(
-    xs[ys == 3, 0],
-    xs[ys == 3, 1],
-    c="C3",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="s",
-)
-pl.scatter(
-    xt2[ys == 0, 0],
-    xt2[ys == 0, 1],
-    c="C7",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="o",
-)
-pl.scatter(
-    xt2[ys == 3, 0],
-    xt2[ys == 3, 1],
-    c="C7",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="s",
-)
-
-# plot classifiers
-ax = pl.axis()
-for i in range(nb):
-    xclass = get_lda_interp(xs, xt2, alphalist[i], 2)
-    pl.plot(
-        xclass[:, 0],
-        xclass[:, 1],
-        color="C2",
-        alpha=alpha0 + (1 - alpha0) * alphalist[i],
-        zorder=0,
-        linewidth=2,
+    # Plot the S shape
+    ax.plot(
+        rotated_curve[:, 1],
+        rotated_curve[:, 0],
+        color=color_S,
+        linewidth=start_length - 2 * i,
+        alpha=1 - i / 8,
     )
-pl.text(
-    1.3,
-    -1.18,
-    r"\bf\textsf{SKADA}",
-    fontsize=80,
+
+# Plot the S shape
+ax.plot(curve[:, 1], curve[:, 0], color=color_S, linewidth=2)
+# Draw point in the square 0 10 and choose the class of the point depending on the curve
+n_dots = 200
+max = 2
+min = 1
+dots = rng.rand(n_dots, 2) * 10
+dots_class = []
+dots_keep = []
+for i, dot in enumerate(dots):
+    # get the closest x axis point of the curve of dot
+    closest_point = curve[np.argmin(np.abs(curve[:, 0] - dot[0]))]
+    if (abs(closest_point[1] - dot[1]) < max) & (abs(closest_point[1] - dot[1]) > min):
+        dots_keep.append(dot)
+        if closest_point[1] > dot[1]:
+            dots_class.append(1)
+        else:
+            dots_class.append(0)
+
+dots_keep = np.array(dots_keep)
+dots_class = np.array(dots_class)
+# Plot the dots
+# choose the cmap with two colors
+s = 150
+alpha = 0.9
+linewidths = 1
+
+# plot the dots with different color and shape depending of the class
+ax.scatter(
+    dots_keep[dots_class == 0, 1],
+    dots_keep[dots_class == 0, 0],
+    color=color_1,
+    marker="o",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
+
+ax.scatter(
+    dots_keep[dots_class == 1, 1],
+    dots_keep[dots_class == 1, 0],
+    color=color_0,
+    marker="s",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
+
+dots_rotated = np.dot(dots_keep - rotation_center, rotation_matrix) + rotation_center
+
+ax.scatter(
+    dots_rotated[dots_class == 0, 1],
+    dots_rotated[dots_class == 0, 0],
+    color=color_unlabel,
+    marker="o",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
+
+ax.scatter(
+    dots_rotated[dots_class == 1, 1],
+    dots_rotated[dots_class == 1, 0],
+    color=color_unlabel,
+    marker="s",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
+
+# Hide axes
+ax.axis("off")
+
+fontsize = 250
+y_axis = 1.5
+ax.text(
+    10,
+    y_axis,
+    r"\bf\textsf{K}",
     usetex=True,
-    zorder=0.5,
-    color=(0.2, 0.2, 0.2),
+    fontsize=fontsize,
+    color="black",
+)
+ax.text(
+    16,
+    y_axis,
+    r"\bf\textsf{A}",
+    usetex=True,
+    fontsize=fontsize,
+    color="black",
+)
+ax.text(
+    22.5,
+    y_axis,
+    r"\bf\textsf{D}",
+    usetex=True,
+    fontsize=fontsize,
+    color=color_1,
+)
+ax.text(
+    28,
+    y_axis,
+    r"\bf\textsf{A}",
+    usetex=True,
+    fontsize=fontsize,
+    color=color_0,
 )
 
-pl.axis(ax)
-pl.axis("off")
 
-pl.savefig("skada_logo_full.svg", bbox_inches="tight", dpi=400)
-pl.savefig("skada_logo_full.png", bbox_inches="tight", dpi=400)
+# Save the figure
+plt.savefig(
+    "skada_logo_full.svg",
+    dpi=300,
+    bbox_inches="tight",
+)
+plt.savefig(
+    "skada_logo_full.pdf",
+    dpi=300,
+    bbox_inches="tight",
+)
 
 # %%
-# Plot the full logo in white
-# ===========================
 
-# target data for full logo
-xt2 = xt.copy()
-xt2[:, 0] += 6.3
+# Create a figure and axis
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.set_xlim(-1, 11)
+ax.set_ylim(-2, 11.2)
+
+# Define the control points for the Bézier curve
+points = np.array([[2, 8], [4, 10], [6, 0], [8, 2]])
 
 
-nb = 10
-alpha0 = 0.2
-alphalist = np.linspace(0, 1, nb)
+# Generate the S shape
+t_values = np.linspace(-0.3, 1.3, 100)
+curve = np.array([bezier_curve(t, points) for t in t_values])
 
-pl.figure(2, (9, 1.5))
-pl.clf()
-alpha = 0.7
+start_length = 10
+for i, angle in enumerate(range(0, 50, 10)):
+    rotation_center = np.mean(curve, axis=0)
+    rotation_angle = np.radians(angle)
+    rotation_matrix = np.array(
+        [
+            [np.cos(rotation_angle), -np.sin(rotation_angle)],
+            [np.sin(rotation_angle), np.cos(rotation_angle)],
+        ]
+    )
+    rotated_curve = np.dot(curve - rotation_center, rotation_matrix) + rotation_center
 
-# plot samples
-pl.scatter(
-    xs[ys == 0, 0],
-    xs[ys == 0, 1],
-    c="w",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
+    # Plot the S shape
+    ax.plot(
+        rotated_curve[:, 1],
+        rotated_curve[:, 0],
+        color=color_S,
+        linewidth=start_length - 2 * i,
+        alpha=1 - i / 8,
+    )
+
+# Plot the S shape
+ax.plot(curve[:, 1], curve[:, 0], color=color_S, linewidth=start_length)
+# Draw point in the square 0 10 and choose the class of the point depending on the curve
+
+# Plot the dots
+
+# plot the dots with different color and shape depending of the class
+ax.scatter(
+    dots_keep[dots_class == 0, 1],
+    dots_keep[dots_class == 0, 0],
+    color=color_1,
     marker="o",
-)
-pl.scatter(
-    xs[ys == 3, 0],
-    xs[ys == 3, 1],
-    c="w",
-    cmap="tab10",
-    vmax=10,
+    s=s,
     alpha=alpha,
-    label="Source data",
-    marker="s",
-)
-pl.scatter(
-    xt2[ys == 0, 0],
-    xt2[ys == 0, 1],
-    c="w",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="o",
-)
-pl.scatter(
-    xt2[ys == 3, 0],
-    xt2[ys == 3, 1],
-    c="w",
-    cmap="tab10",
-    vmax=10,
-    alpha=alpha,
-    label="Source data",
-    marker="s",
+    linewidths=linewidths,
 )
 
-# plot classifiers
-ax = pl.axis()
-for i in range(nb):
-    xclass = get_lda_interp(xs, xt2, alphalist[i], 2)
-    pl.plot(
-        xclass[:, 0],
-        xclass[:, 1],
+ax.scatter(
+    dots_keep[dots_class == 1, 1],
+    dots_keep[dots_class == 1, 0],
+    color=color_0,
+    marker="s",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
+
+dots_rotated = np.dot(dots_keep - rotation_center, rotation_matrix) + rotation_center
+
+ax.scatter(
+    dots_rotated[dots_class == 0, 1],
+    dots_rotated[dots_class == 0, 0],
+    color=color_unlabel,
+    marker="o",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
+
+ax.scatter(
+    dots_rotated[dots_class == 1, 1],
+    dots_rotated[dots_class == 1, 0],
+    color=color_unlabel,
+    marker="s",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
+
+# Hide axes
+ax.axis("off")
+
+
+# Save the figure
+plt.savefig(
+    "skada_logo.svg",
+    dpi=300,
+    bbox_inches="tight",
+)
+plt.savefig(
+    "skada_logo.pdf",
+    dpi=300,
+    bbox_inches="tight",
+)
+
+# %%
+# Create a figure and axis
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.set_xlim(-1, 11)
+ax.set_ylim(-2, 11.2)
+
+# Define the control points for the Bézier curve
+points = np.array([[2, 8], [4, 10], [6, 0], [8, 2]])
+
+
+# Function to calculate a point on a Bézier curve given t (0 <= t <= 1)
+def bezier_curve(t, points):
+    n = len(points) - 1
+    return sum(comb(n, i) * (1 - t) ** (n - i) * t**i * points[i] for i in range(n + 1))
+
+
+# Generate the S shape
+t_values = np.linspace(-0.3, 1.3, 100)
+curve = np.array([bezier_curve(t, points) for t in t_values])
+
+start_length = 10
+for i, angle in enumerate(range(0, 50, 10)):
+    rotation_center = np.mean(curve, axis=0)
+    rotation_angle = np.radians(angle)
+    rotation_matrix = np.array(
+        [
+            [np.cos(rotation_angle), -np.sin(rotation_angle)],
+            [np.sin(rotation_angle), np.cos(rotation_angle)],
+        ]
+    )
+    rotated_curve = np.dot(curve - rotation_center, rotation_matrix) + rotation_center
+
+    # Plot the S shape
+    ax.plot(
+        rotated_curve[:, 1],
+        rotated_curve[:, 0],
         color="w",
-        alpha=alpha0 + (1 - alpha0) * alphalist[i],
-        zorder=0,
-        linewidth=2,
+        linewidth=start_length - 2 * i,
+        alpha=1 - i / 8,
     )
-pl.text(
-    1.3,
-    -1.18,
-    r"\bf\textsf{SKADA}",
-    fontsize=80,
-    usetex=True,
-    zorder=0.5,
+
+# Plot the S shape
+ax.plot(curve[:, 1], curve[:, 0], color="w", linewidth=2)
+
+ax.scatter(
+    dots_keep[dots_class == 0, 1],
+    dots_keep[dots_class == 0, 0],
     color="w",
-    alpha=0.9,
+    marker="o",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
 )
 
-pl.axis(ax)
-pl.axis("off")
+ax.scatter(
+    dots_keep[dots_class == 1, 1],
+    dots_keep[dots_class == 1, 0],
+    color="w",
+    marker="s",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
 
-pl.savefig("skada_logo_full_white.png", transparent=True, bbox_inches="tight", dpi=400)
-pl.savefig("skada_logo_full_white.svg", transparent=True, bbox_inches="tight", dpi=400)
+dots_rotated = np.dot(dots_keep - rotation_center, rotation_matrix) + rotation_center
+
+ax.scatter(
+    dots_rotated[dots_class == 0, 1],
+    dots_rotated[dots_class == 0, 0],
+    color="w",
+    marker="o",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
+
+ax.scatter(
+    dots_rotated[dots_class == 1, 1],
+    dots_rotated[dots_class == 1, 0],
+    color="w",
+    marker="s",
+    s=s,
+    alpha=alpha,
+    linewidths=linewidths,
+)
+
+# Hide axes
+ax.axis("off")
+
+fontsize = 250
+y_axis = 1.5
+ax.text(
+    10,
+    y_axis,
+    r"\bf\textsf{K}",
+    usetex=True,
+    fontsize=fontsize,
+    color="w",
+)
+ax.text(
+    16,
+    y_axis,
+    r"\bf\textsf{A}",
+    usetex=True,
+    fontsize=fontsize,
+    color="w",
+)
+ax.text(
+    22.5,
+    y_axis,
+    r"\bf\textsf{D}",
+    usetex=True,
+    fontsize=fontsize,
+    color="w",
+)
+ax.text(
+    28,
+    y_axis,
+    r"\bf\textsf{A}",
+    usetex=True,
+    fontsize=fontsize,
+    color="w",
+)
+
+
+# Save the figure
+plt.savefig("skada_logo_full_white.svg", dpi=300, bbox_inches="tight", transparent=True)
+plt.savefig("skada_logo_full_white.pdf", dpi=300, bbox_inches="tight", transparent=True)
