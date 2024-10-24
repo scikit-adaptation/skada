bdfae9ef99e523912298ded83952576aaab228fb
Rémi Flamary <remi.flamary@gmail.com>
2024-10-24 15:49:40 +0200

    [MRG] Subsampling transformer (#259)
    * try again
    
    * stuff
    
    * A few quick fixes
    
    * Change the order of rng init
    
    * test with full pipeline
    
    * working pipe
    
    * aupdate test
    
    * import SubsampleTransformer
    
    * update to more general parameter and use of sklearn spliutter
    
    * add stratified smapler
    
    * upate init file
    
    * better tests
    
    * better test
    
    ---------
    
    Co-authored-by: Oleksii Kachaiev <kachayev@gmail.com>
    Co-authored-by: Antoine Collas <contact@antoinecollas.fr>

--- skada/__init__.py
+++ skada/__init__.py
@@ -61,6 +61,7 @@ from ._ot import (
     OTLabelProp,
     JCPOTLabelPropAdapter,
     JCPOTLabelProp)
+from .transformers import SubsampleTransformer, DomainStratifiedSubsampleTransformer
 from ._self_labeling import DASVMClassifier
 from ._pipeline import make_da_pipeline
 from .utils import source_target_split, per_domain_split
@@ -130,6 +131,9 @@ __all__ = [
     "solve_jdot_classification",
     "JDOTClassifier",
     "OTLabelPropAdapter",
+
+    "SubsampleTransformer",
+    "DomainStratifiedSubsampleTransformer",
 
     "make_da_pipeline",
 
