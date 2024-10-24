8e72df4ebf3397d99f9208a5483e7a947cbdca01
Yanis Lalou <57602694+YanisLalou@users.noreply.github.com>
2024-10-24 14:23:33 +0200

    [WIP] Add CAN Method (#251)
    * First commit for CAN
    
    * Update ref + docs
    
    * Add a callback to CAN to compute the kmeans centroids only one time per epoch
    
    * change spherical-kmean compute_similiraties to compute_disimiliraties
    
    * Change cosine dissimilarity function
    
    * Compute kmeans centroids once per epoch
    
    * Add X normalization
    
    * Change in test deep divergence can reg value
    
    * To compute centroids, switch from computing mean to sum

--- skada/deep/tests/test_deep_divergence.py
+++ skada/deep/tests/test_deep_divergence.py
@@ -10,7 +10,7 @@ pytest.importorskip("torch")
 import numpy as np
 
 from skada.datasets import make_shifted_datasets
-from skada.deep import DAN, DeepCoral
+from skada.deep import CAN, DAN, DeepCoral
 from skada.deep.modules import ToyModule2D
 
 
@@ -101,4 +101,51 @@ def test_dan(sigmas):
 
     history = method.history_
 
+    assert history[0]["train_loss"] > history[-1]["train_loss"]
+
+
+@pytest.mark.parametrize(
+    "sigmas, distance_threshold, class_threshold",
+    [
+        ([0.1, 0.2], 0.5, 3),
+        (None, 0.5, 3),
+        ([0.1, 0.2], 0.2, 5),
+    ],
+)
+def test_can(sigmas, distance_threshold, class_threshold):
+    module = ToyModule2D()
+    module.eval()
+
+    n_samples = 10
+    dataset = make_shifted_datasets(
+        n_samples_source=n_samples,
+        n_samples_target=n_samples,
+        shift="concept_drift",
+        noise=0.1,
+        random_state=42,
+        return_dataset=True,
+    )
+
+    method = CAN(
+        ToyModule2D(),
+        reg=0.01,
+        sigmas=sigmas,
+        distance_threshold=distance_threshold,
+        class_threshold=class_threshold,
+        layer_name="dropout",
+        batch_size=10,
+        max_epochs=10,
+        train_split=None,
+    )
+
+    X, y, sample_domain = dataset.pack_train(as_sources=["s"], as_targets=["t"])
+    method.fit(X.astype(np.float32), y, sample_domain)
+
+    X_test, y_test, sample_domain_test = dataset.pack_test(as_targets=["t"])
+
+    y_pred = method.predict(X_test.astype(np.float32), sample_domain_test)
+
+    assert y_pred.shape[0] == X_test.shape[0]
+
+    history = method.history_
     assert history[0]["train_loss"] > history[-1]["train_loss"]
