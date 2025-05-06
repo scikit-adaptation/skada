e4b7266871973be3b4e6d2c745cb8e26bbef42e9
Aruket <aruket.ipa@gmail.com>
2025-05-06 19:54:52 +0200

    beginners guide creation and "how to" fix

---
+++ examples/plot_beginners_guide.py
@@ -0,0 +1,89 @@
+"""
+Beginners Guide
+===============
+
+This is an introduction page to SKADA: SciKit Adaptation for beginners.
+SKADA is an open-source library focusing on domain adaptation methods that goes
+hand in hand with scikit-learn.
+
+In this page, we will present the key concepts and methods for starting to use SKADA
+and how to use them.
+
+* :ref:`Shifted dataset creation<Creating a shifted dataset>`:
+    * :ref:`Covariate shift<Dataset with covariate shift`
+    * :ref:`Target shift<Dataset with target shift>`
+    * :ref:`Conditional shift<Dataset with conditional shift>`
+    * :ref:`Subspace shift<Dataset with subspace shift>`
+
+* :ref:`Methods<Adaptation methods>`
+    * :ref:`Reweighting<Source dataset reweighting>`
+    * :ref:`Mapping<Source to target mapping>`
+    * :ref:`Subspace<Subspace mapping>`
+
+* :ref:`Summary<Results summary>`
+
+"""
+
+# Author: Maxence Barneche
+#
+# License: BSD 3-Clause
+
+# %%
+# Creating a shifted dataset
+# --------------------------
+#
+# In SKADA,
+
+# %%
+# Dataset with covariate shift
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#
+
+# %%
+# Dataset with target shift
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#
+
+# %%
+# Dataset with conditional shift
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#
+
+# %%
+# Dataset with subspace shift
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#
+
+# %%
+# Adaptation methods
+# ------------------
+#
+#
+
+# %%
+# Source dataset reweighting
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#
+
+# %%
+# Source to target mapping
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#
+
+# %%
+# Subspace mapping
+# ~~~~~~~~~~~~~~~~
+#
+#
+
+# %%
+# Results summary
+# ---------------
+#
+#
