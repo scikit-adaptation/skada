1616ce3e1ff66f7bc9ed4ee61c44f25b1f7497c3
mbarneche <aruket.ipa@gmail.com>
2025-05-22 14:22:03 +0200

    fixed merging and empty initialisation

--- skada/deep/_deep_da_dataset.py
+++ skada/deep/_deep_da_dataset.py
@@ -18,108 +18,153 @@ class DeepDADataset(Dataset):
         dataset=None,
         y=None,
         sample_domain=None,
-        sample_weights=None,
+        sample_weight=None,
         device="cpu",
     ):
         self.X = _EMPTY_
         self.y = _EMPTY_
         self.sample_domain = _EMPTY_
-        self.sample_weights = _EMPTY_
+        self.sample_weight = _EMPTY_
         self.has_y = _EMPTY_
-        self.has_weights = bool(len(self.sample_weights))
+        self.has_weights = bool(len(self.sample_weight))
         self.device = device
 
         if sample_domain is None:
             sample_domain = _DEFAULT_SAMPLE_DOMAIN_
 
         if dataset is not None:
-            if isinstance(dataset, dict) and len(dataset):
-                self._if_given_dict(dataset)
+            if isinstance(dataset, dict):
+                if len(dataset):
+                    self._if_given_dict(dataset, y, sample_domain, sample_weight)
             elif isinstance(dataset, DeepDADataset):
-                self.merge(dataset)
+                self.merge(dataset, keep_weights=True, out=False)
             else:
                 try:
-                    X = check_array(dataset, allow_nd=True, ensure_2d=False)
+                    X = check_array(
+                        dataset,
+                        allow_nd=True,
+                        ensure_2d=False,
+                        ensure_min_samples=False,
+                    )
                     X = to_tensor(X, device)
                 except TypeError:
                     raise TypeError(
-                        "Invalid dataset type. Expected a dict of the form "
+                        "Invalid dataset representation. Expected a dict of the form "
                         "{'X', 'y'(optional), 'sample_domain'(optional), "
-                        "'sample_weights'(optional)},"
+                        "'sample_weight'(optional)},"
                         " another DeepDADataset instance or "
                         "a data type convertible to a torch Tensor"
                         f"but found {type(dataset)} instead."
                     )
-                self._true_init(X, y, sample_domain, sample_weights)
+                if not isinstance(sample_domain, int) and not len(sample_domain):
+                    sample_domain = _DEFAULT_SAMPLE_DOMAIN_
+                self._true_init(X, y, sample_domain, sample_weight)
 
-    def _if_given_dict(self, dataset: dict):
-        if "X" not in dataset or "sample_domain" not in dataset:
-            raise ValueError(
-                "dataset represented as dict should contain both 'X' and"
-                " 'sample_domain' keys."
-            )
-        sample_domain = dataset["sample_domain"]
+        assert (len(self.X) == len(self.y) == len(self.sample_domain)) and (
+            len(self.X) == len(self.sample_weight) or not self.has_weights
+        ), (
+            "Every input data X should have a domain associated, a label (if there is "
+            "no label, it must be represented by torch.nan) and, optionally, a weight. "
+            "If you see this message, please report the issue in the "
+            "original GitHub repository at https://github.com/scikit-adaptation/skada/"
+        )
+
+    def _if_given_dict(self, dataset: dict, y, sample_domain, sample_weight):
+        if "X" not in dataset:
+            raise KeyError("dataset represented as dict should contain 'X' key.")
+
         X = dataset["X"]
-        y = dataset.get("y")
-        sample_weights = dataset.get("sample_weights")
+        y = dataset.get("y", y)
+        sample_domain = dataset.get("sample_domain", sample_domain)
+        sample_weight = dataset.get("sample_weight", sample_weight)
 
-        dataset = DeepDADataset(X, y, sample_domain, sample_weights, device=self.device)
+        dataset = DeepDADataset(X, y, sample_domain, sample_weight, device=self.device)
+        self.merge(dataset, keep_weights=True, out=False)
 
-        self.merge(dataset)
-
-    def _true_init(self, X, y, sample_domain, sample_weights):
+    def _true_init(self, X, y, sample_domain, sample_weight):
         if isinstance(sample_domain, int):
             sample_domain = torch.full_like(X, sample_domain)
         else:
-            sample_domain = check_array(sample_domain, allow_nd=True, ensure_2d=False)
+            sample_domain = check_array(
+                sample_domain, allow_nd=True, ensure_2d=False, ensure_min_samples=False
+            )
             sample_domain = to_tensor(sample_domain, self.device)
 
-        if y is None:
-            y = torch.full_like(X, _NO_LABEL_)
+        if y is None or not len(y):
+            y = torch.full_like(X, _NO_LABEL_, dtype=torch.float)
             has_y = torch.full_like(X, False)
         else:
             y = check_array(
-                y, allow_nd=True, force_all_finite="allow-nan", ensure_2d=False
+                y,
+                allow_nd=True,
+                ensure_all_finite="allow-nan",
+                ensure_2d=False,
+                ensure_min_samples=False,
             )
             y = to_tensor(y, self.device)
             has_y = y != _NO_LABEL_
 
-        if sample_weights is None:
-            sample_weights = _EMPTY_
+        if sample_weight is None or not len(sample_weight):
+            sample_weight = _EMPTY_
             has_weights = False
         else:
-            sample_weights = check_array(sample_weights)
-            sample_weights = to_tensor(sample_weights, device=self.device)
-            has_weights = True
+            sample_weight = check_array(
+                sample_weight, allow_nd=True, ensure_min_samples=False, ensure_2d=False
+            )
+            sample_weight = to_tensor(sample_weight, device=self.device)
+            has_weights = bool(len(sample_weight))
 
         self.X = X
         self.y = y
         self.sample_domain = sample_domain
-        self.sample_weights = sample_weights
+        self.sample_weight = sample_weight
         self.has_y = has_y
         self.has_weights = has_weights
 
-    def merge(self, other: "DeepDADataset", keep_weights=False):
+    def merge(self, other: "DeepDADataset", keep_weights=False, out=True):
         if isinstance(other, DeepDADataset):
-            self.X = torch.cat((self.X, other.X))
-            self.y = torch.cat((self.y, other.y))
+            X = torch.cat((self.X, other.X))
+            y = torch.cat((self.y, other.y))
+            sample_domain = torch.cat((self.sample_domain, other.sample_domain))
             if keep_weights:
-                # printing a warning that the concatenated weights should form a
-                # proper distribution would be nice here
-                self.sample_weights = torch.cat(
-                    (self.sample_weights, other.sample_weights)
-                )
+                sample_weight = torch.cat((self.sample_weight, other.sample_weight))
             else:
-                self.sample_weights = _EMPTY_
-            self.sample_domain = torch.cat((self.sample_domain, other.sample_domain))
-            self.has_y = torch.cat((self.has_y, other.has_y))
-            self.has_weights = bool(len(self.sample_weights))
+                sample_weight = _EMPTY_
+            if out:
+                return DeepDADataset(X, y, sample_domain, sample_weight, X.device)
+            else:
+                self.X = X
+                self.y = y
+                self.sample_domain = sample_domain
+                self.sample_weight = sample_weight
+                self.has_y = torch.cat((self.has_y, other.has_y))
+                self.has_weights = bool(len(self.sample_weight))
+                self.device = X.device
 
         else:
             raise TypeError("Can only merge two instances of DeepDADataset")
 
-    def __add__(self, other: "DeepDADataset"):
-        self.merge(other)
+    def __add__(self, other):
+        if isinstance(other, dict):
+            try:
+                other = DeepDADataset(other)
+            except KeyError:
+                raise ValueError(
+                    "Can only add an instance of "
+                    "DeepDADataset with another one, a convertible dict or an iterable"
+                    " with convertible data representation"
+                )
+        elif not isinstance(other, DeepDADataset):
+            try:
+                other = DeepDADataset(*other)
+            except TypeError:
+                raise ValueError(
+                    "Can only add an instance of "
+                    "DeepDADataset with another one, a convertible dict or an iterable"
+                    " with convertible data representation"
+                )
+
+        return self.merge(other)
 
     def __len__(self):
         return len(self.X)
@@ -127,32 +172,32 @@ class DeepDADataset(Dataset):
     def __getitem__(self, index):
         if not isinstance(index, (int, slice)):
             raise TypeError(
-                "DeepDADataset indices must be integers," f" not {type(index)}"
+                f"DeepDADataset indices must be int or slice, not {type(index)}"
             )
 
         X = {"X": self.X[index], "sample_domain": self.sample_domain[index]}
 
         if self.has_weights:
-            X["sample_weight"] = self.sample_weights[index]
+            X["sample_weight"] = self.sample_weight[index]
 
         return X, self.y[index]
 
     def __repr__(self):
         rep = f"DeepDADataset(\n{self.X},\n\n{self.y},\n\n{self.sample_domain}"
         if self.has_weights:
-            rep += f",\n\n{self.sample_weights}"
+            rep += f",\n\n{self.sample_weight}"
         rep += "\n    )"
         return rep
 
     def as_dict(self):
         dataset = {"X": self.X, "y": self.y, "sample_domain": self.sample_domain}
         if self.has_weights:
-            dataset["sample_weights"] = self.sample_weights
+            dataset["sample_weight"] = self.sample_weight
         return dataset
 
     def as_arrays(self, return_weights=False):
         if self.has_weights and return_weights:
-            return self.X, self.y, self.sample_domain, self.sample_weights
+            return self.X, self.y, self.sample_domain, self.sample_weight
         else:
             return self.X, self.y, self.sample_domain
 
